68242b0d8a898bc422cad7b9bfc7d854cdd03c14
Birk Magnussen <birk.magnussen@online.de>
2019-05-15 20:40:27 +0200

    TB-34: Added Methods required to implement AbstractRESTRequest

--- src/main/java/de/uniks/se1ss19teamb/rbsg/request/AbstractRESTRequest.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/request/AbstractRESTRequest.java
@@ -4,9 +4,15 @@ import org.json.simple.JSONObject;
 
 public abstract class AbstractRESTRequest implements RESTRequest{
 	
+	private static final String url = "https://rbsg.uniks.de/api";
+	
 	private JSONObject response = null;
 	
 	protected abstract JSONObject buildJSON();
+	
+	protected abstract String getHTTPMethod(); //"get", "post", "delete", "put"
+	
+	protected abstract String getEndpoint(); //"/user", "/user/login", etc.
 	
 	@Override
 	public void sendRequest() {
