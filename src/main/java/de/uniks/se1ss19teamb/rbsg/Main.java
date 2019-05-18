e2c427078971f7c5f2ca4314fc4072470181a0fc
rohhullah <rohhullah98@gmail.com>
2019-05-18 17:33:29 +0200

    register screen finished

--- src/main/java/de/uniks/se1ss19teamb/rbsg/Main.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("LoginScreen");
+        Parent child = FXMLLoader.load(getClass().getResource("register.fxml"));
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
