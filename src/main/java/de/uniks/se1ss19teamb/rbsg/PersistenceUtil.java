f10afd1b6b5ff63417484140271812558fa0e47f
0gain <gianluca.voss@gmx.de>
2019-05-20 12:16:46 +0200

    tabs to spaces changed imports

--- src/main/java/de/uniks/se1ss19teamb/rbsg/PersistenceUtil.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/PersistenceUtil.java
@@ -10,51 +10,51 @@ import java.nio.file.Paths;
 
 public class PersistenceUtil {
 
-	public static final String SAVEGAME_YAML = "./savegame.yaml";
-	private YamlIdMap yamlIdMap = new YamlIdMap(Game.class.getPackage().getName());
+    public static final String SAVEGAME_YAML = "./savegame.yaml";
+    private YamlIdMap yamlIdMap = new YamlIdMap(Game.class.getPackage().getName());
 
-	public void save(Game game) {
+    public void save(Game game) {
 
-		String yaml = yamlIdMap.encode(game);
+        String yaml = yamlIdMap.encode(game);
 
-		try {
+        try {
 
-			File file = new File(SAVEGAME_YAML);
+            File file = new File(SAVEGAME_YAML);
 
-			if(!file.exists()) {
-					file.createNewFile();
-			}
+            if(!file.exists()) {
+                    file.createNewFile();
+            }
 
-			Files.write(Paths.get(file.toURI()), yaml.getBytes("UTF-16"));
+            Files.write(Paths.get(file.toURI()), yaml.getBytes("UTF-16"));
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private Game load() {
-		File file = new File(SAVEGAME_YAML);
+    private Game load() {
+        File file = new File(SAVEGAME_YAML);
 
-		if(!file.exists()) {
-			return null;
-		}
+        if(!file.exists()) {
+            return null;
+        }
 
-		Game game = new Game();
+        Game game = new Game();
 
-		try {
-			byte[] bytes = Files.readAllBytes(Paths.get(file.toURI()));
-			String yaml = new String(bytes, "UTF-16");
-			yamlIdMap.decode(yaml, game);
-			return game;
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(file.toURI()));
+            String yaml = new String(bytes, "UTF-16");
+            yamlIdMap.decode(yaml, game);
+            return game;
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	public Game loader() {
-		PersistenceUtil p = new PersistenceUtil();
-		return p.load();
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public Game loader() {
+        PersistenceUtil p = new PersistenceUtil();
+        return p.load();
+    }
 }
--- src/main/test/java/de.uniks.se1ss19.teamb.rbsg/testsave/SaveandLoadTest.java
+++ src/main/test/java/de.uniks.se1ss19.teamb.rbsg/testsave/SaveandLoadTest.java
@@ -7,40 +7,40 @@ import de.uniks.se1ss19teamb.rbsg.testmodel.Player;
 import de.uniks.se1ss19teamb.rbsg.testmodel.Unit;
 import org.junit.jupiter.api.Test;
 
+
 import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class SaveandLoadTest {
 
-	@Test
-	public void saveandloadTest() {
-		
-		Game game = new Game();
-		Player alice = new Player().setName("Alice");
-		Platform p1 = new Platform();
-		Platform p2 = new Platform();
-		game.withPlayers(alice).withPlatforms(p1, p2);
-		p1.setCapacity(5);
-		p2.setCapacity(3);
-		alice.withPlatforms(p1, p2);
-		for(int i = 0; i < 5; i++) {
-			alice.withUnits(new Unit());
-			alice.getUnits().get(i).setPlatform(p1);
-		}
-		p1.withNeighbors(p2);
+    @Test
+    public void saveandloadTest() {
+        
+        Game game = new Game();
+        Player alice = new Player().setName("Alice");
+        Platform p1 = new Platform();
+        Platform p2 = new Platform();
+        game.withPlayers(alice).withPlatforms(p1, p2);
+        p1.setCapacity(5);
+        p2.setCapacity(3);
+        alice.withPlatforms(p1, p2);
+        for(int i = 0; i < 5; i++) {
+            alice.withUnits(new Unit());
+            alice.getUnits().get(i).setPlatform(p1);
+        }
+        p1.withNeighbors(p2);
 
-		PersistenceUtil p = new PersistenceUtil();
-		p.save(game);
+        PersistenceUtil p = new PersistenceUtil();
+        p.save(game);
 
-		Game loadedGame = p.loader();
+        Game loadedGame = p.loader();
 
-		assertEquals(loadedGame.getPlayers().get(0).getName(), game.getPlayers().get(0).getName());
-		assertEquals(loadedGame.getPlatforms().get(0).getCapacity(), game.getPlatforms().get(0).getCapacity());
-		assertEquals(loadedGame.getPlatforms().get(1).getCapacity(), game.getPlatforms().get(1).getCapacity());
+        assertEquals(loadedGame.getPlayers().get(0).getName(), game.getPlayers().get(0).getName());
+        assertEquals(loadedGame.getPlatforms().get(0).getCapacity(), game.getPlatforms().get(0).getCapacity());
+        assertEquals(loadedGame.getPlatforms().get(1).getCapacity(), game.getPlatforms().get(1).getCapacity());
 
-		File file = new File("./savegame.yaml");
-		file.delete();
-	}
+        File file = new File("./savegame.yaml");
+        file.delete();
+    }
 }
