bd39404e2b134825ef6f741574709efc31b3f40d
Birk Magnussen <birk.magnussen@online.de>
2019-05-20 15:25:33 +0200

    Spaces instead of Tabs

--- src/main/java/de/uniks/se1ss19teamb/rbsg/request/LogoutUserRequest.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/request/LogoutUserRequest.java
@@ -4,29 +4,29 @@ import org.json.simple.JSONObject;
 
 public class LogoutUserRequest extends AbstractRESTRequest {
 
-	private String userToken;
-	
-	public LogoutUserRequest(String userToken) {
-		this.userToken = userToken;
-	}
-	
-	@Override
-	protected JSONObject buildJSON() {
-		return null;
-	}
+   private String userToken;
+   
+   public LogoutUserRequest(String userToken) {
+      this.userToken = userToken;
+   }
+   
+   @Override
+   protected JSONObject buildJSON() {
+      return null;
+   }
 
-	@Override
-	protected String getHTTPMethod() {
-		return "get";
-	}
+   @Override
+   protected String getHTTPMethod() {
+      return "get";
+   }
 
-	@Override
-	protected String getEndpoint() {
-		return "/user/logout";
-	}
+   @Override
+   protected String getEndpoint() {
+      return "/user/logout";
+   }
 
-	@Override
-	protected String getUserToken() {
-		return userToken;
-	}
+   @Override
+   protected String getUserToken() {
+      return userToken;
+   }
 }
