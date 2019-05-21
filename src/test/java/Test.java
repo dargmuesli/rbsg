c4d3bf6d0386d3d378f88d6ab11974e81c568229
Deadbymidnight <admin@Deadbymidnight.com>
2019-05-21 15:02:49 +0200

    Added Test file

---
+++ src/test/java/Test.java
@@ -0,0 +1,22 @@
+import de.uniks.se1ss19teamb.rbsg.crypto.CipherController;
+import org.junit.Assert;
+
+public class Test {
+
+    @org.junit.Test
+    public void Test(){
+
+        String msg = "gå til helvete";
+        CipherController cip = new CipherController();
+        cip.encryptMessage(msg);
+
+        String returnedmsg = cip.decryptMessage();
+
+        Assert.assertEquals("gå til helvete",returnedmsg);
+
+        System.out.println();
+        System.out.println(""+returnedmsg);
+
+
+    }
+}
