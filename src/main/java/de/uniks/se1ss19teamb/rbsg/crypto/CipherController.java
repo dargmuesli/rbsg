7259f0cad61b34d0323a262f6bb9dd6643b2ee92
Deadbymidnight <admin@Deadbymidnight.com>
2019-05-21 13:31:54 +0200

    Added secrets.txt for encryption and decryption

--- src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherController.java
@@ -33,8 +33,7 @@ public class CipherController {
 
 
             //4.Store the secret message into text file for now
-            //TODO:filelocation need to be mentioned
-            FileWriter fw = new FileWriter("");
+            FileWriter fw = new FileWriter("src/main/resources/de.uniks.se1ss19teamb.rbsg/secrets.txt");
             fw.write(Base64.encodeBase64String(secret));
             fw.close();
 
@@ -58,14 +57,15 @@ public class CipherController {
     }
 
     //decrypt the message
-    public void decryptMessage(){
-        System.out.println("decrypt message : ");
+    public String decryptMessage(){
+        //System.out.println("decrypt message : ");
+
+        String decrypted_message = "NOT_DECRYPTED!!!!" ;
 
         //1.Read the encrypted message
         //here files location need to be given
         try {
-            //TODO:filelocation need be mentioned
-            FileReader fr = new FileReader("");
+            FileReader fr = new FileReader("src/main/resources/de.uniks.se1ss19teamb.rbsg/secrets.txt");
             BufferedReader br = new BufferedReader(fr);
 
             //2.Construct the encrypted message
@@ -81,10 +81,12 @@ public class CipherController {
 
             //4.Decrypt the message and print it on the users screen
             byte[] recovered_message = CipherUtils.decrypt(CipherConstant.privateKey,recSecret);
-            System.out.println(new String(recovered_message,"UTF8"));
 
+            decrypted_message =(new String(recovered_message,"UTF8"));
             br.close();
             fr.close();
+
+            //return (new String(recovered_message,"UTF8"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -100,6 +102,8 @@ public class CipherController {
         } catch (IllegalBlockSizeException e) {
             e.printStackTrace();
         }
+        return decrypted_message;
+
     }
 
 }
