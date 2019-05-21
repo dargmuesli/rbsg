8041691cfbbd602bce6ff485f26680b5f90a13d1
Deadbymidnight <admin@Deadbymidnight.com>
2019-05-21 14:58:59 +0200

    Corrected errors

--- src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherUtils.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherUtils.java
@@ -39,14 +39,14 @@ public class CipherUtils {
 
     //encrypt using public key and returns it as byte
     public static byte[] encrypt(PublicKey key, byte[] plaintext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1ANDGF1Padding");
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
         cipher.init(Cipher.ENCRYPT_MODE,key);
         return cipher.doFinal(plaintext);
     }
 
     //decrypts the key using privatekey and returns as Byte
     public static byte[] decrypt(PrivateKey key,byte[] ciphertext) throws NoSuchAlgorithmException,NoSuchPaddingException,InvalidKeyException,IllegalBlockSizeException,BadPaddingException{
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1ANDGF1Padding");
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");
         cipher.init(Cipher.DECRYPT_MODE,key);
         return cipher.doFinal(ciphertext);
     }
