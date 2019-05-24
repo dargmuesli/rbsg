fd9b08de096c681374224de6b8f0c4af8b1f9375
Hombr3 <vadjka.92@mail.ru>
2019-05-24 22:47:44 +0200

    TB.39 thought prozess

--- src/main/java/de/uniks/se1ss19teamb/rbsg/chat/Chat.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/chat/Chat.java
@@ -1,11 +1,21 @@
 package de.uniks.se1ss19teamb.rbsg.chat;
 
+import javax.websocket.OnMessage;
+import java.util.ArrayList;
+
 public class Chat {
+
+    ArrayList<Message> chatLog = new ArrayList<Message>();
+    // ArrayList<User> users = new ArrayList<User>();
+
     /*
-     TODO:
-       connect to server
-       get messages
-       receive messages
+    add to chatLog:
+     -on Message sent
+     -on Message received
+     -on System message received
+
+    Messages acn be sent to either all user or specific users (chatMode)
+    System messages are received by all Users
      */
 
 }
