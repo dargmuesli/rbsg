94a01bf9a6a9649093a55162d141726a5ade8394
Patrick <pennyplayerg@gmail.com>
2019-05-24 17:21:22 +0200

    Error Handling function
    Adds function to the ErrorPopupController.
    Small changes to the ErrorPopup.fxml

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ErrorPopupController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ErrorPopupController.java
@@ -1,4 +1,33 @@
 package de.uniks.se1ss19teamb.rbsg;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ErrorPopupController {
+
+    @FXML
+    Label errorLabel;
+
+    String errorMessage;
+
+    class ErrorMessageThread implements Runnable {
+        @Override
+        public void run()
+        {
+            errorLabel.setText(errorMessage);
+            errorLabel.setVisible(true);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            errorLabel.setVisible(false);
+        }
+    }
+
+    public void displayErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+        Thread errorThread = new Thread(new ErrorMessageThread());
+        errorThread.start();
+    }
 }
