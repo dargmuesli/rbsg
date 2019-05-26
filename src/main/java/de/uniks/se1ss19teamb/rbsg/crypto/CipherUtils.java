50c93ed67bee18bbd271860c53f7ed83760b40c7
Deadbymidnight <admin@Deadbymidnight.com>
2019-05-26 14:26:09 +0200

    Update code

--- src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherUtils.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherUtils.java
@@ -15,27 +15,23 @@ import java.security.spec.X509EncodedKeySpec;
 
 public class CipherUtils {
 
-
     public static byte[] readFileBytes(String filename) throws IOException{
         Path path =  Paths.get(filename);
         return Files.readAllBytes(path);
     }
 
     //read Strings and generates public key for the given Strings
-
     public static PublicKey readPublicKey(String filename) throws InvalidKeySpecException, NoSuchAlgorithmException,IOException{
         X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(readFileBytes(filename));
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePublic(publicSpec);
     }
 
-
     //reads Strings and generates Private key for the given Strings
     public static PrivateKey readPrivateKey(String filename) throws IOException,NoSuchAlgorithmException,InvalidKeySpecException{
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(readFileBytes(filename));
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePrivate(keySpec);
-
     }
 
     //encrypts using public key and returns as Byte
