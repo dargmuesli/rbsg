22f9065a13ba07543f4349bed858fe13e9079a49
Hombr3 <vadjka.92@mail.ru>
2019-05-26 11:42:28 +0200

    Adding package serialize

---
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/serialize/SerializeUtils.java
@@ -0,0 +1,127 @@
+package de.uniks.se1ss19teamb.rbsg.serialize;
+
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+
+public class SerializeUtils{
+
+  /*
+  public static class Game {
+
+    //Fields defined by Server
+    private long joinedPlayers;
+    private String name;
+    private String id;
+    private long neededPlayers;
+
+    public long getJoinedPlayers() {
+      return joinedPlayers;
+    }
+    public void setJoinedPlayers(Long joinedPlayers) {
+      this.joinedPlayers = joinedPlayers;
+    }
+    public String getName() {
+      return name;
+    }
+    public void setName(String name) {
+      this.name = name;
+    }
+    public String getId() {
+      return id;
+    }
+    public void setId(String id) {
+      this.id = id;
+    }
+    public long getNeededPlayers() {
+      return neededPlayers;
+    }
+    public void setNeededPlayers(long neededPlayers) {
+      this.neededPlayers = neededPlayers;
+    }
+  }
+  /*
+  // Method 1
+  public static void serializeGame(String file, Game game){
+    JSONObject json = new JSONObject();
+    json.put("joinedPlayers", game.getJoinedPlayers());
+    json.put("name", game.getName());
+    json.put("id", game.getId());
+    json.put("neededPlayers", game.getNeededPlayers());
+
+    try{
+      FileWriter jsonFileWriter = new FileWriter(file);
+      jsonFileWriter.write(json.toJSONString());
+      jsonFileWriter.flush();
+      jsonFileWriter.close();
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  public static Game deSerializeGameFromFile(String file, Game game){
+    JSONParser parser = new JSONParser();
+
+    try {
+      FileReader fileReader = new FileReader(file);
+      JSONObject json = (JSONObject) parser.parse(fileReader);
+
+      long joinedPlayers = (long) json.get("joinedPlayers");
+      String name = (String) json.get("name");
+      String id = (String) json.get("id");
+      long neededPlayers = (long) json.get("neededPlayers");
+
+      game.setJoinedPlayers(joinedPlayers);
+      game.setName(name);
+      game.setId(id);
+      game.setNeededPlayers(neededPlayers);
+    } catch (Exception ex){
+      ex.printStackTrace();
+    }
+    return game;
+  }
+  */
+
+    public static Object deserialize(String fileUrl, Class myClass) {
+        Gson gson = new Gson();
+        try (Reader reader = new FileReader(fileUrl)) {
+            Object fromJson = gson.fromJson(reader, myClass);
+            return fromJson;
+        } catch (IOException e) {
+            if (fileUrl instanceof String ){
+                Object fromJson = gson.fromJson(fileUrl, myClass);
+                return fromJson;
+            } else {
+                e.printStackTrace();
+            }
+        }
+
+        return myClass;
+    }
+
+    public static void serialize(String fileUrl, Object object) {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(fileUrl)) {
+            gson.toJson(object, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String serialize(Object object) {
+        return new Gson().toJson(object);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
