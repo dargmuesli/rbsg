337a78a7619dc5f4da0cb83c3504ad211f7e61f7
Deadbymidnight <admin@Deadbymidnight.com>
2019-05-26 14:16:39 +0200

    Update Code

--- src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherConstant.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/CipherConstant.java
@@ -4,13 +4,11 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public class CipherConstant {
-    //Constant
     static PublicKey publicKey;
     static PrivateKey privateKey;
 
     static {
         try {
-            //CipherConnect.connect();
             publicKey = CipherUtils.readPublicKey("src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/public.der");
             privateKey = CipherUtils.readPrivateKey("src/main/java/de/uniks/se1ss19teamb/rbsg/crypto/Dummy.der");
         } catch (Exception e) {
