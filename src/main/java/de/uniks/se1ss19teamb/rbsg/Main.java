b8b2552f67f518c33e5683531409f98f5d38ac7a
rohhullah <rohhullah98@gmail.com>
2019-05-26 22:12:23 +0200

    minSize changed

--- src/main/java/de/uniks/se1ss19teamb/rbsg/Main.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("LoginScreen");
+        primaryStage.setTitle("RSBG-Team B");
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
