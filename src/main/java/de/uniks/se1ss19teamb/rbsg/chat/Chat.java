7b203c1e5cc1dfc468f4859bc23030a223796a67
Hombr3 <vadjka.92@mail.ru>
2019-05-30 01:36:47 +0200

    Fixing minor style differenses

--- src/main/java/de/uniks/se1ss19teamb/rbsg/chat/Chat.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/chat/Chat.java
@@ -7,58 +7,58 @@ import java.util.Date;
 
 public class Chat {
 
-  private String CHANNEL = "/chat?user=";
-  ChatLogEntry chatLogEntry = new ChatLogEntry();
-  public String userName;
-  public String sendToUser;
-  public String message;
-
-  public Chat (String userName) {
-        this.userName = userName;
-        chatLogEntry.sender = userName;
-    }
-
-  public void setMessage (String message) {
-    this.message = message;
-    chatLogEntry.message = message;
-    chatLogEntry.addToChatLog(this.userName, message);
-  }
-
-  public void sendMessage() {
-    // send Message (all)
-  }
-
-  public void sendMessage(String sendToUser){
-    // send Message (private)
-  }
-
-  public void writeLog(Path path) throws IOException {
-    chatLogEntry.writeLog(path);
-  }
-
-  public static class ChatLogEntry{
-
-    public static ArrayList<String> chatLog = new ArrayList<String>();
-    public Date date = new Date();
-    public String sender;
+    private String CHANNEL = "/chat?user=";
+    ChatLogEntry chatLogEntry = new ChatLogEntry();
+    public String userName;
+    public String sendToUser;
     public String message;
 
+    public Chat (String userName) {
+                this.userName = userName;
+                chatLogEntry.sender = userName;
+        }
 
-    public ChatLogEntry() {}
-
-    public void addToChatLog(String sender, String message){
-      chatLog.add(sender + " sagte: \"" + message + "\" " + "am " + new Date());
+    public void setMessage (String message) {
+        this.message = message;
+        chatLogEntry.message = message;
+        chatLogEntry.addToChatLog(this.userName, message);
     }
 
-    public void writeLog (Path path) throws IOException {
-      PrintWriter out = new PrintWriter(path.toString());
-      out.println("Chat log seit: " + date + "\n");
-      for (String s : chatLog) {
-        out.println(s);
-      }
-      out.close();
+    public void sendMessage() {
+        // send Message (all)
     }
 
-  }
+    public void sendMessage(String sendToUser) {
+        // send Message (private)
+    }
+
+    public void writeLog(Path path) throws IOException {
+        chatLogEntry.writeLog(path);
+    }
+
+    public static class ChatLogEntry {
+
+        public static ArrayList<String> chatLog = new ArrayList<String>();
+        public Date date = new Date();
+        public String sender;
+        public String message;
+
+
+        public ChatLogEntry() {}
+
+        public void addToChatLog(String sender, String message) {
+            chatLog.add(sender + " sagte: \"" + message + "\" " + "am " + new Date());
+        }
+
+        public void writeLog (Path path) throws IOException {
+            PrintWriter out = new PrintWriter(path.toString());
+            out.println("Chat log seit: " + date + "\n");
+            for (String s : chatLog) {
+                out.println(s);
+            }
+            out.close();
+        }
+
+    }
 
 }
