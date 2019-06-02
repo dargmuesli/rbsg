31bc6788d858a2e77db9848f1ae4c065f800829e
Hombr3 <vadjka.92@mail.ru>
2019-06-02 18:26:27 +0200

    TB - 39: Removing CHANNEL, receiver and message as it is not needed Adding login, logout and chatSocket from sockets package Adding login request aswell as chatSocket request to Chat constructor and a password parameter Adding disconnect method Removing addToChatLog from sendMessage methods as it is not needed Adding receiveMessages method, which will fill the chatLog

--- src/main/java/de/uniks/se1ss19teamb/rbsg/chat/Chat.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/chat/Chat.java
@@ -1,40 +1,67 @@
 package de.uniks.se1ss19teamb.rbsg.chat;
 
+import de.uniks.se1ss19teamb.rbsg.request.LoginUserRequest;
+import de.uniks.se1ss19teamb.rbsg.request.LogoutUserRequest;
+import de.uniks.se1ss19teamb.rbsg.sockets.ChatSocket;
 import de.uniks.se1ss19teamb.rbsg.util.SerializeUtils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class Chat {
+public class Chat  {
 
-    private String CHANNEL = "/chat?user=";
     ArrayList<ChatLogEntry> chatLog = new ArrayList<ChatLogEntry>();
     public String sender;
-    public String receiver;
-    public String message;
+    LoginUserRequest login;
+    LogoutUserRequest logout;
+    ChatSocket chatSocket;
+    Path path = Paths.get("src/main/resources/de/uniks/se1ss19teamb/rbsg/chatLog.txt");
 
-    public Chat (String sender) {
+    public Chat (String sender, String password) throws IOException {
         this.sender = sender;
+        // Login
+        this.login = new LoginUserRequest(sender, password);
+        login.sendRequest();
+
+        this.chatSocket = new ChatSocket(sender, login.getUserKey());
+
+        this.chatSocket.connect();
+
+        this.receiveMessages();
+    }
+
+    public void disconnect() throws IOException {
+        writeLog(this.path);
+        this.chatSocket.disconnect();
+        this.logout = new LogoutUserRequest(this.login.getUserKey());
+        this.logout.sendRequest();
     }
 
     public void sendMessage(String message) {
         // TODO send action (All)
-        addToChatLog(this.sender, message);
+        this.chatSocket.sendMessage(message);
     }
 
-    public void sendMessage(String receiver, String message) {
+    public void sendMessage(String message, String receiver) {
         // TODO send action (private)
-        addToChatLog(this.sender, receiver, message);
+        this.chatSocket.sendPrivateMessage(message, receiver);
     }
 
-    public void addToChatLog(String sender, String message) {
-        chatLog.add(new ChatLogEntry(sender, message));
+    public void receiveMessages() {
+        this.chatSocket.registerChatMessageHandler((message, from, isPrivate) -> {
+            addToChatLog(message, from, this.sender);
+        });
     }
 
-    public void addToChatLog(String sender, String receiver, String message) {
-        chatLog.add(new ChatLogEntry(sender, receiver, message));
+    public void addToChatLog(String message, String sender) {
+        chatLog.add(new ChatLogEntry(message, sender));
+    }
+
+    public void addToChatLog(String message, String sender, String receiver) {
+        chatLog.add(new ChatLogEntry(message, sender, receiver));
     }
 
     public void writeLog (Path path) throws IOException {
