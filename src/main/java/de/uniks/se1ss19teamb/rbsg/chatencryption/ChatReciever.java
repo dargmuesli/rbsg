76e1d96c2965264570517b6bf7dc49f14360e026
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-03 01:52:02 +0200

    First version

---
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/chatencryption/ChatReciever.java
@@ -0,0 +1,42 @@
+package de.uniks.se1ss19teamb.rbsg.chatencryption;
+
+import java.io.IOException;
+
+public class ChatReciever extends Thread {
+    String prompt;
+    ChatCommunication chat;
+    String serverAdd;
+
+    public ChatReciever(ChatCommunication chat, String prompt, String serverAdd) {
+        this.chat = chat;
+        this.serverAdd = serverAdd;
+        this.prompt = prompt;
+    }
+
+    public void run() {
+        ChatMessage recieved;
+        int count = 0;
+        String publicKey;
+        try {
+            while ((recieved = chat.receiveMessage()) != null) {
+                if (count == 0) {
+                    publicKey = recieved.text;
+                    chat.anotherPulbic = publicKey;
+                } else if (count == 1) {
+                    if (!(prompt.equalsIgnoreCase("Client : "))) {
+                        System.out.println("Connected to Server: " + serverAdd + "\nStreams Established. Ready to Chat .. ");
+                    } else {
+                        System.out.println("Keys Exchanged");
+
+                    }
+                } else {
+                    System.out.println("This message was sent at : ");
+                    System.out.println("This message was recieved at : ");
+                }
+                count++;
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
