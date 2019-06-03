2540dc2a894fc5cfa9eee0319d3c818cd27ec0d3
0gain <gianluca.voss@gmx.de>
2019-06-03 20:57:24 +0200

    fxml for create game adds a setOnAction in MainController fixes spelling error in register

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/MainController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/MainController.java
@@ -1,21 +1,31 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
+import com.jfoenix.controls.JFXButton;
 import de.uniks.se1ss19teamb.rbsg.util.ErrorHandler;
 import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 
 public class MainController {
+    
     @FXML
     private AnchorPane mainScreen;
     
     @FXML
     private AnchorPane errorContainer;
+    
+    @FXML
+    private JFXButton btnCreate;
+    
+    @FXML
+    private ToggleGroup playerNumberToggleGroup;
     
     public void initialize() {
         mainScreen.setOpacity(0);
@@ -33,6 +43,13 @@ public class MainController {
             
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    
+    public void setOnAction(ActionEvent event) {
+        
+        if(event.getSource().equals(btnCreate)) {
+        
         }
     }
 }
