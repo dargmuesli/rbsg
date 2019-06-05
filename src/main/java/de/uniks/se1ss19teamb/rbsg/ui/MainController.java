1564e7a533b52aecdbc1dbd3938137343515d005
rohhullah <rohhullah98@gmail.com>
2019-06-06 00:03:45 +0200

    First try for View Player

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/MainController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/MainController.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
 public class MainController {
@@ -16,11 +17,19 @@ public class MainController {
     
     @FXML
     private AnchorPane errorContainer;
+
+    @FXML
+    private ListView playerListView;
     
     public void initialize() {
         mainScreen.setOpacity(0);
         UserInterfaceUtils.makeFadeInTransition(mainScreen);
-        
+
+        playerListView.setStyle("-fx-control-inner-background: #2A2E37;");
+        playerListView.getItems().add("test player");
+        playerListView.getItems().add("test player 2");
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass()
                 .getResource("/de/uniks/se1ss19teamb/rbsg/ErrorPopup.fxml"));
         try {
