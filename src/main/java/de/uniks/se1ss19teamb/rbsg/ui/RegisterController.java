0e5c35934ec3b297ac4d0db16fe4701f746d9687
rohhullah <rohhullah98@gmail.com>
2019-06-05 20:31:56 +0200

    Update

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/RegisterController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/RegisterController.java
@@ -1,50 +1,98 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import de.uniks.se1ss19teamb.rbsg.request.RegisterUserRequest;
 import de.uniks.se1ss19teamb.rbsg.util.ErrorHandler;
+import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
 
 import java.io.IOException;
 
-import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Duration;
 
 public class RegisterController {
     
     @FXML
     private AnchorPane registerScreen;
-
+    
+    @FXML
+    private JFXButton btnCancel;
+    
+    @FXML
+    private JFXButton btnConfirm;
+    
     @FXML
     AnchorPane errorContainer;
-
+    
+    @FXML
+    private JFXTextField userName;
+    
+    @FXML
+    private JFXTextField password;
+    
+    @FXML
+    private JFXTextField confirmPassword;
+    
+    private ErrorPopupController controller;
+    
+    private ErrorHandler errorHandler;
+    
     public void initialize() {
         registerScreen.setOpacity(0);
-        makeFadeInTransition();
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass()
-                .getResource("/de/uniks/se1ss19teamb/rbsg/ErrorPopup.fxml"));
+        UserInterfaceUtils.makeFadeInTransition(registerScreen);
+        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "/de/uniks/se1ss19teamb/rbsg/ErrorPopup.fxml"));
         try {
             Parent parent = fxmlLoader.load();
-            // controller not used yet, but it's good to have it for later purposes.
-            ErrorPopupController controller = fxmlLoader.getController();
-            ErrorHandler errorHandler = new ErrorHandler();
-            errorHandler.setErrorPopupController(controller);
             errorContainer.getChildren().add(parent);
-
+    
+            controller = fxmlLoader.getController();
+            errorHandler = new ErrorHandler();
+            errorHandler.setErrorPopupController(controller);
+            
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
+        }      
     }
-
-    private void makeFadeInTransition() {
-        FadeTransition fadeTransition = new FadeTransition();
-        fadeTransition.setDuration(Duration.millis(1000));
-        fadeTransition.setNode(registerScreen);
-        fadeTransition.setFromValue(0);
-        fadeTransition.setToValue(1);
-        fadeTransition.play();
+    
+    @FXML
+    void setOnAction(ActionEvent event) throws IOException {
+        if (event.getSource().equals(btnCancel)) {
+            UserInterfaceUtils.makeFadeOutTransition(
+                    "/de/uniks/se1ss19teamb/rbsg/login.fxml", registerScreen);
+        }
+        if (event.getSource().equals(btnConfirm)) {
+            if (!userName.getText().isEmpty()
+                    && !password.getText().isEmpty()
+                    && !confirmPassword.getText().isEmpty()) {
+                
+                if (password.getText().equals(confirmPassword.getText())) {
+                    RegisterUserRequest register = new RegisterUserRequest(
+                            userName.getText(), password.getText());
+                    
+                    register.sendRequest();
+                    if (register.getSuccessful()) {
+                        UserInterfaceUtils.makeFadeOutTransition(
+                                "/de/uniks/se1ss19teamb/rbsg/login.fxml", registerScreen);
+                        
+                        errorHandler.sendError("Registrierung erfolgreich!");
+                    } /*else {
+                        errorHandler.sendError("Entschuldigung.
+                         Es ist etwas bei der Registrierung schief gelaufen.
+                        Bitte versuchen Sie er erneut.");
+                    }*/
+                } else {
+                    errorHandler.sendError("Die Passwörter sind verschieden!");
+                }
+            
+            } else {
+                errorHandler.sendError("Bitte geben Sie etwas ein.");
+            }
+        }
     }
 }
---
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/util/UserInterfaceUtils.java
@@ -0,0 +1,75 @@
+package de.uniks.se1ss19teamb.rbsg.util;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javafx.animation.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class UserInterfaceUtils {
+    
+    public static void makeFadeOutTransition(String path, AnchorPane node) {
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(node);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(event -> {
+            try {
+                fadeNextScene(path, node);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        fadeTransition.play();
+    }
+    
+    public static void makeFadeInTransition(AnchorPane node) {
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(node);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
+    }
+    
+    public static void fadeNextScene(String path, AnchorPane stage) throws IOException {
+        Parent nextScene = FXMLLoader.load(UserInterfaceUtils.class.getResource(path));
+        Scene scene = new Scene(nextScene);
+        Stage currentStage = (Stage) stage.getScene().getWindow();
+        currentStage.setScene(scene);
+    }
+    
+    private void slideNextScene(String path, int value, AnchorPane pane) throws IOException {
+        Random r = new Random();
+        boolean randomValue = r.nextBoolean();
+        Parent root = FXMLLoader.load(getClass().getResource(path));
+        Timeline timeline;
+        KeyValue keyValue;
+
+        if (randomValue) {
+            root.translateYProperty().setValue(-400);
+            
+            timeline = new Timeline();
+            keyValue = new KeyValue(root.translateYProperty(),0, Interpolator.EASE_IN);
+        } else {
+            root.translateXProperty().setValue(-value);
+            
+            timeline = new Timeline();
+            keyValue = new KeyValue(root.translateXProperty(),0,Interpolator.EASE_IN);
+        }
+
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1),keyValue);
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setScene(scene);
+    }
+}
