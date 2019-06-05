53b2b1c06dbc8c5ea72481aba28289f44e32fac6
rohhullah <rohhullah98@gmail.com>
2019-06-06 01:13:23 +0200

    second try of design

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/MainController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/MainController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.AnchorPane;
 
@@ -22,18 +23,14 @@ public class MainController {
 
     @FXML
     private ListView playerListView;
+
+    @FXML
+    private ScrollPane scrollPanePlayer;
     
     public void initialize() {
         mainScreen.setOpacity(0);
         UserInterfaceUtils.makeFadeInTransition(mainScreen);
-
-        playerListView.setStyle("-fx-control-inner-background: #2A2E37;");
-        playerListView.getItems().add("test player");
-        playerListView.getItems().add("test player 2");
-        playerListView.getItems().add("test player 3");
-        playerListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
-
+        setPlayerListView();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass()
                 .getResource("/de/uniks/se1ss19teamb/rbsg/ErrorPopup.fxml"));
         try {
@@ -47,5 +44,16 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setPlayerListView(){
+        scrollPanePlayer.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPanePlayer.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPanePlayer.setStyle("-fx-background-color:transparent;");
+        playerListView.setStyle("-fx-control-inner-background: #2A2E37;" + "-fx-background-insets: 0 ;");
+        //(Test player) TODO : put the real player into the playerListView
+        playerListView.getItems().add("test player");
+        playerListView.getItems().add("test player 2");
+        playerListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     }
 }
