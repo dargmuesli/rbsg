3af9b72bfb0d62a4b9dbb41ee54c3d4ca298467a
0gain <gianluca.voss@gmx.de>
2019-06-06 18:17:30 +0200

    buttons in login work on enter renames Eventhandlers cleans code

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/LoginController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/LoginController.java
@@ -12,11 +12,14 @@ import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
 
 import java.io.*;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class LoginController {
@@ -50,31 +53,8 @@ public class LoginController {
     
     public static String userKey;
     
-    public static String getUserKey() {
-        return userKey;
-    }
-    
-    public static void setUserKey(String key) {
-        userKey = key;
-    }
-    
     public void initialize() {
-        File file = new File(USER_DATA);
-        if (file.exists()) {
-            try {
-                FileReader reader = new FileReader(file);
-                BufferedReader br = new BufferedReader(reader);
-                String userName = br.readLine();
-                String password = br.readLine();
-                this.userName.setText(userName);
-                this.password.setText(password);
-            } catch (FileNotFoundException e) {
-                errorHandler.sendError("Userdaten konnten nicht geladen werden.");
-            } catch (IOException e) {
-                errorHandler.sendError("Userdaten konnten nicht geladen werden.");
-            }
-            rememberLogin.setSelected(true);
-        }
+        loadData();
         loginScreen.setOpacity(0);
         UserInterfaceUtils.makeFadeInTransition(loginScreen);
         
@@ -94,46 +74,100 @@ public class LoginController {
         }
     }
     
+    
+    
     @FXML
-    void setOnAction(ActionEvent event) throws IOException {
+    void EventHandler(ActionEvent event) throws IOException {
+        
         if (event.getSource().equals(btnLogin)) {
-            //slideNextScene("main.fxml",100);
-            if (!userName.getText().isEmpty() && !password.getText().isEmpty()) {
-                LoginUserRequest login = new LoginUserRequest(
-                        userName.getText(), password.getText());
-                login.sendRequest();
-                if (login.getSuccessful()) {
-                    File file = new File(USER_DATA);
-                    if (file.exists()) {
-                        file.delete();
-                    }
-                    if (rememberLogin.isSelected()) {
-                        file.createNewFile();
-                        FileWriter writer = new FileWriter(file);
-                        writer.write(userName.getText());
-                        writer.write(System.getProperty("line.separator"));
-                        writer.write(password.getText());
-                        writer.flush();
-                    }
-                    
-                    setUserKey(login.getUserKey());
-                    UserInterfaceUtils.makeFadeOutTransition(
-                            "/de/uniks/se1ss19teamb/rbsg/main.fxml", loginScreen);
-                    errorHandler.sendError("Login erfolgreich!");
-                    
-                } else {
-                    errorHandler.sendError("Login fehlgeschlagen.");
-                }
-            } else {
-                errorHandler.sendError("Bitte geben Sie etwas ein.");
-            }
+            login();
         }
-        
         if (event.getSource().equals(btnRegistration)) {
-            //slideNextScene("register.fxml",400);
-            UserInterfaceUtils.makeFadeOutTransition(
-                    "/de/uniks/se1ss19teamb/rbsg/register.fxml", loginScreen);
+            goToRegister();
         }
+    }
+    
+    public void KeyEventHandler(KeyEvent keyEvent) throws IOException {
         
+        if(keyEvent.getSource().equals(btnLogin) && keyEvent.getCode().equals(KeyCode.ENTER)) {
+            login();
+        }
+        if(keyEvent.getSource().equals(btnRegistration) && keyEvent.getCode().equals(KeyCode.ENTER)) {
+            goToRegister();
+        }
+        if (keyEvent.getSource().equals(rememberLogin) && keyEvent.getCode().equals(KeyCode.ENTER)) {
+            rememberLogin.setSelected(true);
+        }
+    }
+    
+    private void login() throws IOException {
+        //slideNextScene("main.fxml",100);
+        if (!userName.getText().isEmpty() && !password.getText().isEmpty()) {
+            LoginUserRequest login = new LoginUserRequest(
+                    userName.getText(), password.getText());
+            login.sendRequest();
+            if (login.getSuccessful()) {
+                File file = new File(USER_DATA);
+                if (file.exists()) {
+                    file.delete();
+                }
+                if (rememberLogin.isSelected()) {
+                    saveData(file);
+                }
+                
+                setUserKey(login.getUserKey());
+                UserInterfaceUtils.makeFadeOutTransition(
+                        "/de/uniks/se1ss19teamb/rbsg/main.fxml", loginScreen);
+                errorHandler.sendError("Login erfolgreich!");
+                
+            } else {
+                errorHandler.sendError("Login fehlgeschlagen.");
+            }
+        } else {
+            errorHandler.sendError("Bitte geben Sie etwas ein.");
+        }
+    }
+    
+    private void goToRegister() {
+        //slideNextScene("register.fxml",400);
+        UserInterfaceUtils.makeFadeOutTransition(
+                "/de/uniks/se1ss19teamb/rbsg/register.fxml", loginScreen);
+    }
+    
+    private void saveData(File file) throws IOException {
+        file.createNewFile();
+        FileWriter writer = new FileWriter(file);
+        writer.write(userName.getText());
+        writer.write(System.getProperty("line.separator"));
+        writer.write(password.getText());
+        writer.flush();
+    }
+    
+    private void loadData() {
+        File file = new File(USER_DATA);
+        if (file.exists()) {
+            try {
+                FileReader reader = new FileReader(file);
+                BufferedReader br = new BufferedReader(reader);
+                String userName = br.readLine();
+                String password = br.readLine();
+                this.userName.setText(userName);
+                this.password.setText(password);
+            } catch (FileNotFoundException e) {
+                errorHandler.sendError("Userdaten konnten nicht geladen werden.");
+            } catch (IOException e) {
+                errorHandler.sendError("Userdaten konnten nicht geladen werden.");
+            }
+            rememberLogin.setSelected(true);
+            Platform.runLater(() -> btnLogin.requestFocus());
+        }
+    }
+    
+    public static String getUserKey() {
+        return userKey;
+    }
+    
+    public static void setUserKey(String key) {
+        userKey = key;
     }
 }
--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/RegisterController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/RegisterController.java
@@ -62,7 +62,7 @@ public class RegisterController {
     }
     
     @FXML
-    void setOnAction(ActionEvent event) throws IOException {
+    void EventHandler(ActionEvent event) throws IOException {
         if (event.getSource().equals(btnCancel)) {
             UserInterfaceUtils.makeFadeOutTransition(
                     "/de/uniks/se1ss19teamb/rbsg/login.fxml", registerScreen);
