9c7a4cf53428adaedf8852fb48596bb1aac4d2cc
0gain <gianluca.voss@gmx.de>
2019-06-07 13:32:26 +0200

    fixes missing getuserkey

--- src/main/java/de/uniks/se1ss19teamb/rbsg/sockets/ChatSocket.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/sockets/ChatSocket.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatSocket extends AbstractWebSocket {
-
+    
     private String userKey;
     private String userName;
     
     private List<ChatMessageHandler> handlersChat = new ArrayList<>();
-
+    
     private boolean ignoreOwn;
     
     public ChatSocket(String userName, String userKey) {
@@ -22,17 +22,17 @@ public class ChatSocket extends AbstractWebSocket {
         this.userKey = userKey;
         this.userName = userName;
         this.ignoreOwn = ignoreOwn;
-        registerWebSocketHandler((response) -> { 
+        registerWebSocketHandler((response) -> {
             if (response.get("msg") != null) {
                 //TODO Handle error in MSG
                 return;
             }
             
             String from = response.get("from").getAsString();
-            if (this.ignoreOwn && from.equals(userName)) { 
-                return; 
+            if (this.ignoreOwn && from.equals(userName)) {
+                return;
             }
-
+            
             String msg = response.get("message").getAsString();
             boolean isPrivate = response.get("channel").getAsString().equals("private");
             for (ChatMessageHandler handler : handlersChat) {
@@ -45,12 +45,12 @@ public class ChatSocket extends AbstractWebSocket {
     protected String getEndpoint() {
         return "/chat?user=" + userName;
     }
-
+    
     @Override
     public String getUserKey() {
         return userKey;
     }
-
+    
     public String getUserName() {
         return userName;
     }
