df9d55832bf595dde08a24b4a45373fd91ac7354
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-07 14:36:39 +0200

    ChatSocket: Reindent Lines
    Using IntelliJ IDEA with 4 spaces.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/sockets/ChatSocket.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/sockets/ChatSocket.java
@@ -6,18 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChatSocket extends AbstractWebSocket {
-    
+
     private String userKey;
     private String userName;
-    
+
     private List<ChatMessageHandler> handlersChat = new ArrayList<>();
-    
+
     private boolean ignoreOwn;
-    
+
     public ChatSocket(String userName, String userKey) {
         this(userName, userKey, false);
     }
-    
+
     public ChatSocket(String userName, String userKey, boolean ignoreOwn) {
         this.userKey = userKey;
         this.userName = userName;
@@ -27,12 +27,12 @@ public class ChatSocket extends AbstractWebSocket {
                 //TODO Handle error in MSG
                 return;
             }
-            
+
             String from = response.get("from").getAsString();
             if (this.ignoreOwn && from.equals(userName)) {
                 return;
             }
-            
+
             String msg = response.get("message").getAsString();
             boolean isPrivate = response.get("channel").getAsString().equals("private");
             for (ChatMessageHandler handler : handlersChat) {
@@ -40,34 +40,34 @@ public class ChatSocket extends AbstractWebSocket {
             }
         });
     }
-    
+
     @Override
     protected String getEndpoint() {
         return "/chat?user=" + userName;
     }
-    
+
     @Override
     public String getUserKey() {
         return userKey;
     }
-    
+
     public String getUserName() {
         return userName;
     }
-    
+
     //Custom Helpers
-    
+
     public void registerChatMessageHandler(ChatMessageHandler handler) {
         handlersChat.add(handler);
     }
-    
+
     public void sendMessage(String message) {
         JsonObject json = new JsonObject();
         json.addProperty("channel", "all");
         json.addProperty("message", message);
         sendToWebsocket(json);
     }
-    
+
     public void sendPrivateMessage(String message, String target) {
         JsonObject json = new JsonObject();
         json.addProperty("channel", "private");
