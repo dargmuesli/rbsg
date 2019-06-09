0163c0ef1ae0ca254bffbf6ebccd39b3a9feaebf
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-10 01:15:40 +0200

    Set Stage's Minimum Width & Height
    Prevents senseless downscaling.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/Main.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/Main.java
@@ -18,7 +18,11 @@ public class Main extends Application {
         Parent root = FXMLLoader
             .load(getClass().getResource("/de/uniks/se1ss19teamb/rbsg/login.fxml"));
         primaryStage.setTitle("RSBG-Team B");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 800, 650));
+        // values taken from the largest fxml's root container width & height
+        // the minHeight is slightly larger so that the horizontal 1px separator pane is always displayed
+        primaryStage.setMinWidth(533);
+        primaryStage.setMinHeight(621);
         primaryStage.setOnCloseRequest(event -> System.exit(0));
         primaryStage.show();
 
