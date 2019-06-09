cd5d56fe75b0fb6b25a0de12085d92221e756486
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-09 03:09:50 +0200

    Simplify UserInterfaceUtils
    Comment out unused method "slideNextScene".
    Dissolve "fadeNextScene".
    Decrease fade transition duration to 750ms.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/util/UserInterfaceUtils.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/util/UserInterfaceUtils.java
@@ -1,14 +1,10 @@
 package de.uniks.se1ss19teamb.rbsg.util;
 
 import java.io.IOException;
-import java.util.Random;
 
 import javafx.animation.*;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class UserInterfaceUtils {
@@ -16,13 +12,13 @@ public class UserInterfaceUtils {
 
     public static void makeFadeOutTransition(String path, AnchorPane node) {
         FadeTransition fadeTransition = new FadeTransition();
-        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setDuration(Duration.millis(750));
         fadeTransition.setNode(node);
         fadeTransition.setFromValue(1);
         fadeTransition.setToValue(0);
         fadeTransition.setOnFinished(event -> {
             try {
-                fadeNextScene(path, node);
+                node.getScene().setRoot(FXMLLoader.load(UserInterfaceUtils.class.getResource(path)));
             } catch (IOException e) {
                 errorHandler.sendError("Übergang in die nächste Szene konnte nicht ausgeführt werden!");
                 e.printStackTrace();
@@ -40,38 +36,31 @@ public class UserInterfaceUtils {
         fadeTransition.play();
     }
     
-    public static void fadeNextScene(String path, AnchorPane stage) throws IOException {
-        Parent nextScene = FXMLLoader.load(UserInterfaceUtils.class.getResource(path));
-        Scene scene = new Scene(nextScene);
-        Stage currentStage = (Stage) stage.getScene().getWindow();
-        currentStage.setScene(scene);
-    }
-    
-    private void slideNextScene(String path, int value, AnchorPane pane) throws IOException {
-        Random r = new Random();
-        boolean randomValue = r.nextBoolean();
-        Parent root = FXMLLoader.load(getClass().getResource(path));
-        Timeline timeline;
-        KeyValue keyValue;
-
-        if (randomValue) {
-            root.translateYProperty().setValue(-400);
-            
-            timeline = new Timeline();
-            keyValue = new KeyValue(root.translateYProperty(),0, Interpolator.EASE_IN);
-        } else {
-            root.translateXProperty().setValue(-value);
-            
-            timeline = new Timeline();
-            keyValue = new KeyValue(root.translateXProperty(),0,Interpolator.EASE_IN);
-        }
-
-        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1),keyValue);
-        timeline.getKeyFrames().add(keyFrame);
-        timeline.play();
-
-        Scene scene = new Scene(root);
-        Stage stage = (Stage) pane.getScene().getWindow();
-        stage.setScene(scene);
-    }
+//    private void slideNextScene(String path, int value, AnchorPane pane) throws IOException {
+//        Random r = new Random();
+//        boolean randomValue = r.nextBoolean();
+//        Parent root = FXMLLoader.load(getClass().getResource(path));
+//        Timeline timeline;
+//        KeyValue keyValue;
+//
+//        if (randomValue) {
+//            root.translateYProperty().setValue(-400);
+//
+//            timeline = new Timeline();
+//            keyValue = new KeyValue(root.translateYProperty(),0, Interpolator.EASE_IN);
+//        } else {
+//            root.translateXProperty().setValue(-value);
+//
+//            timeline = new Timeline();
+//            keyValue = new KeyValue(root.translateXProperty(),0,Interpolator.EASE_IN);
+//        }
+//
+//        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1),keyValue);
+//        timeline.getKeyFrames().add(keyFrame);
+//        timeline.play();
+//
+//        Scene scene = new Scene(root);
+//        Stage stage = (Stage) pane.getScene().getWindow();
+//        stage.setScene(scene);
+//    }
 }
