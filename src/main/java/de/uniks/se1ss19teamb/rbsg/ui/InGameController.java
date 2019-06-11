40b7f341c6233045352200ef31875b03a8c73592
Christopher Bartalos <chrisibartalos96@web.de>
2019-06-11 10:06:20 +0200

    basic FXML inGame
    implemented new class inGameController
    implemented new fxml file inGame.fxml

---
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/InGameController.java
@@ -0,0 +1,16 @@
+package de.uniks.se1ss19teamb.rbsg.ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class InGameController {
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("inGame.fxml"));
+        primaryStage.setTitle("InGameScreen");
+        primaryStage.setScene(new Scene(root, 400, 600));
+        primaryStage.show();
+    }
+}
