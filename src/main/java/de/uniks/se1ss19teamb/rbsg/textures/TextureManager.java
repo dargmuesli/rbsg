2882eccd9df8c8de79202863b4abc6c67404db07
Birk Magnussen <birk.magnussen@online.de>
2019-06-11 20:07:25 +0200

    Checkstyle

--- src/main/java/de/uniks/se1ss19teamb/rbsg/textures/TextureManager.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/textures/TextureManager.java
@@ -7,39 +7,39 @@ import javafx.geometry.Dimension2D;
 import javafx.scene.layout.Pane;
 
 public class TextureManager {
-	
-	private  static TextureManager instance = null;
-	
-	public static void init() {
-		instance = new TextureManager();
-		
-		Texture test = new Texture("Test.png");
-		instance.textures.put("test", test);
-		
-		Texture missing = new Texture("Missing.png");
-		instance.textures.put("missing", missing);
-	}
-	
-	public static Pane getTextureInstance(String toFetch) {
-		return instance.fetchTexture(toFetch).instantiate();
-	}
-	
-	public static Dimension2D getTextureDimensions(String toFetch) {
-		Texture texture = instance.fetchTexture(toFetch);
-		return new Dimension2D(texture.image.getWidth(), texture.image.getHeight());
-	}
-	
-	private Map<String, Texture> textures = new HashMap<>();
-	
-	private TextureManager() { }
-	
-	private Texture fetchTexture(String toFetch) {
-		Texture texture = textures.get(toFetch);
-		
-		if(texture == null) {
-			texture = textures.get("missing");
-		}
-		
-		return texture;
-	}
+    
+    private  static TextureManager instance = null;
+    
+    public static void init() {
+        instance = new TextureManager();
+        
+        Texture test = new Texture("Test.png");
+        instance.textures.put("test", test);
+        
+        Texture missing = new Texture("Missing.png");
+        instance.textures.put("missing", missing);
+    }
+    
+    public static Pane getTextureInstance(String toFetch) {
+        return instance.fetchTexture(toFetch).instantiate();
+    }
+    
+    public static Dimension2D getTextureDimensions(String toFetch) {
+        Texture texture = instance.fetchTexture(toFetch);
+        return new Dimension2D(texture.image.getWidth(), texture.image.getHeight());
+    }
+    
+    private Map<String, Texture> textures = new HashMap<>();
+    
+    private TextureManager() { }
+    
+    private Texture fetchTexture(String toFetch) {
+        Texture texture = textures.get(toFetch);
+        
+        if(texture == null) {
+            texture = textures.get("missing");
+        }
+        
+        return texture;
+    }
 }
--- src/test/java/de/uniks/se1ss19teamb/rbsg/textures/TextureManagerTest.java
+++ src/test/java/de/uniks/se1ss19teamb/rbsg/textures/TextureManagerTest.java
@@ -10,24 +10,24 @@ import javafx.scene.layout.Pane;
 import org.junit.Assert;
 
 public class TextureManagerTest {
-	
-	@Before
-	public void prepareTest() {
-		TextureManager.init();
-	}
-	
-	@Test
-	public void checkLoadedTexture() {
-		Assert.assertEquals(new Dimension2D(64, 64), TextureManager.getTextureDimensions("test"));
-		Assert.assertEquals(new Dimension2D(64, 64), TextureManager.getTextureDimensions("missing"));
-	}
-	
-	@Test
-	public void checkInstancedPanes() {
-		Pane pane = TextureManager.getTextureInstance("test");
-		Assert.assertFalse(pane.getChildren().filtered((node) -> node instanceof ImageView).isEmpty());
-		Assert.assertFalse(((ImageView)pane.getChildren()
-				.stream().filter((node) -> node instanceof ImageView).findAny().get())
-				.getImage() == null);
-	}
+    
+    @Before
+    public void prepareTest() {
+        TextureManager.init();
+    }
+    
+    @Test
+    public void checkLoadedTexture() {
+        Assert.assertEquals(new Dimension2D(64, 64), TextureManager.getTextureDimensions("test"));
+        Assert.assertEquals(new Dimension2D(64, 64), TextureManager.getTextureDimensions("missing"));
+    }
+    
+    @Test
+    public void checkInstancedPanes() {
+        Pane pane = TextureManager.getTextureInstance("test");
+        Assert.assertFalse(pane.getChildren().filtered((node) -> node instanceof ImageView).isEmpty());
+        Assert.assertFalse(((ImageView)pane.getChildren()
+                .stream().filter((node) -> node instanceof ImageView).findAny().get())
+                .getImage() == null);
+    }
 }
