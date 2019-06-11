67e75fee4d8ef4f11c84ca025b08b03c75ca7b9d
Birk Magnussen <birk.magnussen@online.de>
2019-06-11 23:00:12 +0200

    Checkstyle

--- src/main/java/de/uniks/se1ss19teamb/rbsg/sound/Sound.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/sound/Sound.java
@@ -5,33 +5,31 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class Sound {
-	
-	protected boolean keepInRAM;
-	
-	protected Object sound;
-	
-	protected Sound(String classPath, boolean keepInRAM) {
-		String url = this.getClass().getResource(classPath).toExternalForm();
-		this.keepInRAM = keepInRAM;
-		
-		if(keepInRAM) {
-			sound = new AudioClip(url);
-		}
-		else {
-			sound = new Media(url);			
-		}
-	}
-	
-	public void play(float balance) {
-		if(keepInRAM) {
-			((AudioClip) sound).setBalance(balance);
-			((AudioClip) sound).play();
-		}
-		else {
-			MediaPlayer player = new MediaPlayer((Media) sound);
-			player.setBalance(balance);
-			player.play();
-		}
-	}
-	
+    
+    protected boolean keepInRam;
+    
+    protected Object sound;
+    
+    protected Sound(String classPath, boolean keepInRam) {
+        String url = this.getClass().getResource(classPath).toExternalForm();
+        this.keepInRam = keepInRam;
+        
+        if (keepInRam) {
+            sound = new AudioClip(url);
+        } else {
+            sound = new Media(url);            
+        }
+    }
+    
+    public void play(float balance) {
+        if (keepInRam) {
+            ((AudioClip) sound).setBalance(balance);
+            ((AudioClip) sound).play();
+        } else {
+            MediaPlayer player = new MediaPlayer((Media) sound);
+            player.setBalance(balance);
+            player.play();
+        }
+    }
+    
 }
