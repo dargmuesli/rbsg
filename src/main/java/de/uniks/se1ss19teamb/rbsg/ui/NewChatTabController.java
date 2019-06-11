203cfa585e174cf7cb2f035799435440b3d1e307
Hombr3 <vadjka.92@mail.ru>
2019-06-11 11:34:54 +0200

    Making private tabs closable

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/NewChatTabController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/NewChatTabController.java
@@ -1,8 +1,7 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 public class NewChatTabController {
 
@@ -18,5 +17,16 @@ public class NewChatTabController {
     @FXML
     public void initialize() {
         textArea.appendText("Private Chat started!\n");
+        addContextMenu();
+
+    }
+
+    private void addContextMenu() {
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem closeMenuItem = new MenuItem("close");
+        closeMenuItem.setOnAction((e) -> newTab.getTabPane().getTabs().remove(newTab));
+        contextMenu.getItems().add(closeMenuItem);
+        newTab.setContextMenu(contextMenu);
+
     }
 }
