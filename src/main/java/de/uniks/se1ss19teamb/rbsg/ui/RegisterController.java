011015867474091ee4e4dc699748db1fd2c39ecb
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-13 01:39:25 +0200

    Add User Data Persistence For The Registration Screen

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/RegisterController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/RegisterController.java
@@ -3,16 +3,23 @@ package de.uniks.se1ss19teamb.rbsg.ui;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import de.uniks.se1ss19teamb.rbsg.model.UserData;
+import de.uniks.se1ss19teamb.rbsg.request.LoginUserRequest;
 import de.uniks.se1ss19teamb.rbsg.request.RegisterUserRequest;
 import de.uniks.se1ss19teamb.rbsg.util.ErrorHandler;
+import de.uniks.se1ss19teamb.rbsg.util.SerializeUtils;
 import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
 
+import java.io.File;
 import java.io.IOException;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,6 +29,7 @@ public class RegisterController {
     private static final Logger logger = LogManager.getLogger();
     @FXML
     AnchorPane errorContainer;
+    private ErrorHandler errorHandler = ErrorHandler.getErrorHandler();
     @FXML
     private AnchorPane registerScreen;
     @FXML
@@ -33,10 +41,36 @@ public class RegisterController {
     @FXML
     private JFXPasswordField password;
     @FXML
-    private ErrorHandler errorHandler = ErrorHandler.getErrorHandler();
     private JFXPasswordField passwordRepeat;
+    private UserData userData;
 
     public void initialize() {
+        // load user data
+        userData = UserData.loadUserData(errorHandler);
+
+        if (userData == null) {
+            errorHandler.sendError("User data couldn't be deserialized!");
+            return;
+        }
+
+        username.setText(userData.getRegisterUsername());
+        password.setText(userData.getRegisterPassword());
+        passwordRepeat.setText(userData.getRegisterPasswordRepeat());
+
+        Platform.runLater(() -> {
+            if (username.getText().equals("")) {
+                username.requestFocus();
+            } else if (password.getText().equals("")) {
+                password.requestFocus();
+            } else if (passwordRepeat.getText().equals("")) {
+                passwordRepeat.requestFocus();
+            } else {
+                btnConfirm.requestFocus();
+            }
+        });
+
+        UserData.deleteUserData(errorHandler);
+
         registerScreen.setOpacity(0);
         UserInterfaceUtils.makeFadeInTransition(registerScreen);
 
@@ -56,35 +90,66 @@ public class RegisterController {
 
     @FXML
     void eventHandler(ActionEvent event) {
-        if (event.getSource().equals(btnCancel)) {
-            UserInterfaceUtils.makeFadeOutTransition(
-                "/de/uniks/se1ss19teamb/rbsg/fxmls/login.fxml", registerScreen);
-        } else if (event.getSource().equals(btnConfirm)) {
-            if (username.getText().isEmpty()
-                || password.getText().isEmpty()
-                || passwordRepeat.getText().isEmpty()) {
-                errorHandler.sendError("Bitte geben Sie etwas ein.");
-                return;
-            }
-
-            if (!password.getText().equals(passwordRepeat.getText())) {
-                errorHandler.sendError("Die Passwörter sind verschieden!");
-                return;
-            }
-
-            RegisterUserRequest register = new RegisterUserRequest(
-                username.getText(), password.getText());
-            register.sendRequest();
-
-            if (!register.getSuccessful()) {
-                errorHandler.sendError("Die Registrierung ist fehlgeschlagen!");
-            }
-
-            UserInterfaceUtils.makeFadeOutTransition(
-                "/de/uniks/se1ss19teamb/rbsg/fxmls/login.fxml", registerScreen);
-
-            // TODO success
-            errorHandler.sendError("Registrierung erfolgreich!");
+        if (event.getSource().equals(btnConfirm)) {
+            register();
+        } else if (event.getSource().equals(btnCancel)) {
+            goToLogin();
         }
+    }
+
+    @FXML
+    public void onEnter() {
+        register();
+    }
+
+    public void keyEventHandler(KeyEvent keyEvent) {
+        if (keyEvent.getSource().equals(btnConfirm) && keyEvent.getCode().equals(KeyCode.ENTER)) {
+            register();
+        }
+
+        if (keyEvent.getSource().equals(btnCancel)
+            && keyEvent.getCode().equals(KeyCode.ENTER)) {
+            goToLogin();
+        }
+    }
+
+    private void register() {
+        if (username.getText().isEmpty()
+            || password.getText().isEmpty()
+            || passwordRepeat.getText().isEmpty()) {
+            errorHandler.sendError("Bitte geben Sie etwas ein.");
+            return;
+        }
+
+        if (!password.getText().equals(passwordRepeat.getText())) {
+            errorHandler.sendError("Die Passwörter sind verschieden!");
+            return;
+        }
+
+        RegisterUserRequest register = new RegisterUserRequest(
+            username.getText(), password.getText());
+        register.sendRequest();
+
+        if (!register.getSuccessful()) {
+            errorHandler.sendError("Die Registrierung ist fehlgeschlagen!");
+        }
+
+        UserInterfaceUtils.makeFadeOutTransition(
+            "/de/uniks/se1ss19teamb/rbsg/fxmls/login.fxml", registerScreen);
+
+        // TODO success
+        errorHandler.sendError("Registrierung erfolgreich!");
+    }
+
+    private void goToLogin() {
+        // save user data
+        userData.setRegisterUsername(username.getText());
+        userData.setRegisterPassword(password.getText());
+        userData.setRegisterPasswordRepeat(passwordRepeat.getText());
+
+        SerializeUtils.serialize(UserData.USER_DATA_PATH.toString(), userData);
+
+        UserInterfaceUtils.makeFadeOutTransition(
+            "/de/uniks/se1ss19teamb/rbsg/fxmls/login.fxml", registerScreen);
     }
 }
