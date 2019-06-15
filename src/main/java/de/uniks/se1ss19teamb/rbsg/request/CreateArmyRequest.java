5eafb2c4fda0f58b79a64445e0db62d66c8c41f9
Patrick <pennyplayerg@gmail.com>
2019-06-15 21:29:07 +0200

    CreateArmyRequest
    Adds the CreateArmyRequest class.
    This class is used to pass a created army to the server.
    It has to be passed to the class:
    	- The name of the army (String)
    	- The IDs of the units (List<String>)
    	- The userToken (String)

---
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/request/CreateArmyRequest.java
@@ -0,0 +1,48 @@
+package de.uniks.se1ss19teamb.rbsg.request;
+
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+public class CreateArmyRequest extends AbstractRestRequest {
+
+    private String userToken;
+    private String armyName;
+    private List<String> unitIDs;
+
+    public CreateArmyRequest(String armyName, List<String> unitIDs, String userToken) {
+        this.userToken = userToken;
+        this.unitIDs = unitIDs;
+        this.armyName = armyName;
+    }
+
+    @Override
+    protected JsonObject buildJson() {
+        JsonObject json = new JsonObject();
+        json.addProperty("name", armyName);
+        StringBuilder unitIDBuilder = new StringBuilder();
+        for (String id : unitIDs) {
+            unitIDBuilder.append("\"" + id + "\" ");
+        }
+        unitIDBuilder.deleteCharAt(0);
+        unitIDBuilder.delete(unitIDBuilder.length() - 2, unitIDBuilder.length());
+
+        json.addProperty("name", unitIDBuilder.toString());
+        return json;
+    }
+
+    @Override
+    protected String getHttpMethod() {
+        return "post";
+    }
+
+    @Override
+    protected String getEndpoint() {
+        return "/army";
+    }
+
+    @Override
+    protected String getUserToken() {
+        return userToken;
+    }
+}
