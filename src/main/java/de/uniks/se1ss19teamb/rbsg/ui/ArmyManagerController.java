5109595354f874747992b077a171eaa49038fe86
Christopher Bartalos <chrisibartalos96@web.de>
2019-06-15 09:28:51 +0200

    Buttons added
    added buttons to the army manager maybe some are not needed they can be deleted afterwards

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/ArmyManagerController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/ArmyManagerController.java
@@ -1,16 +1,46 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
 import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import de.uniks.se1ss19teamb.rbsg.request.*;
+
 
 public class ArmyManagerController {
     @FXML
     private AnchorPane mainPane;
+    @FXML
+    private JFXButton btnLogout;
+    @FXML
+    private JFXButton btnWhiteMode;
+    @FXML
+    private JFXHamburger ham;
+    @FXML
+    private JFXButton btnBack;
 
     public void initialize() {
+
+        HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(ham);
+        transition.setRate(-1);
+        ham.addEventHandler(MouseEvent.MOUSE_PRESSED, (e)->{
+            transition.setRate(transition.getRate()*-1);
+            transition.play();
+        });
+        if(transition.getRate() == 1){
+            btnLogout.isVisible();
+            btnWhiteMode.isVisible();
+            btnBack.isVisible();
+        }else if(transition.getRate() == -1){
+            btnLogout.setVisible(false);
+            btnWhiteMode.setVisible(false);
+            btnBack.setVisible(false);
+        }
+
         UserInterfaceUtils.makeFadeInTransition(mainPane);
     }
 
@@ -24,4 +54,20 @@ public class ArmyManagerController {
             default:
         }
     }
+
+    public void setOnAction(ActionEvent event){
+        if (event.getSource().equals(btnLogout)) {
+            LogoutUserRequest logout = new LogoutUserRequest(LoginController.getUserKey());
+            logout.sendRequest();
+            if (logout.getSuccessful()) {
+                LoginController.setUserKey(null);
+                UserInterfaceUtils.makeFadeOutTransition(
+                    "/de/uniks/se1ss19teamb/rbsg/fxmls/login.fxml", mainPane);
+            }
+        } else if (event.getSource().equals(btnWhiteMode)) {
+            UserInterfaceUtils.makeFadeOutTransition(
+                "/de/uniks/se1ss19teamb/rbsg/fxmls/armyManager.fxml", mainPane);
+        }
+    }
 }
+
