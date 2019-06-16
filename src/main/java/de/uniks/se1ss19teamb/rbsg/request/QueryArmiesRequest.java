09fdaeee7e47f30ee7a2a3ac0650d6594f1754f0
Patrick <pennyplayerg@gmail.com>
2019-06-16 17:25:51 +0200

    QueryArmiesRequest
    - Adds the QueryArmiesRequest class which gets all armies from the server.
    - Still has to be edited.

---
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/request/QueryArmiesRequest.java
@@ -0,0 +1,60 @@
+package de.uniks.se1ss19teamb.rbsg.request;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import de.uniks.se1ss19teamb.rbsg.model.Army;
+import de.uniks.se1ss19teamb.rbsg.model.Game;
+
+import java.util.ArrayList;
+
+/*
+public class QueryArmiesRequest{
+
+}
+*/
+public class QueryArmiesRequest extends AbstractRestRequest {
+
+    private String userToken;
+
+    public QueryArmiesRequest(String userToken) {
+        this.userToken = userToken;
+    }
+
+    @Override
+    protected JsonObject buildJson() {
+        return null;
+    }
+
+    @Override
+    protected String getHttpMethod() {
+        return "get";
+    }
+
+    @Override
+    protected String getEndpoint() {
+        return "/army";
+    }
+
+    @Override
+    protected String getUserToken() {
+        return userToken;
+    }
+
+    //Custom Request Helper
+
+    public ArrayList<Army> getArmies() {
+        ArrayList<Army> armies = new ArrayList<Army>();
+
+        for (JsonElement g : getResponse().get("data").getAsJsonArray()) {
+            Army current = new Army();
+            JsonObject army = g.getAsJsonObject();
+            current.setId(army.get("id").getAsString());
+            current.setName(army.get("name").getAsString());
+            //TODO: Check the String and split the IDs to a new String list.
+            String units = army.get("units").getAsString();
+            armies.add(current);
+        }
+
+        return armies;
+    }
+}
