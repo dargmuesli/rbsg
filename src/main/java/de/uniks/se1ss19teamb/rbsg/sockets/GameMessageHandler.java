e170c92da5650cb7917a2ed48ad1f4674adb0d6f
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-26 02:48:53 +0200

    Minor Fixes
    Makes GameMessageHandler public.
    Adds "playerLeftGame" event to SystemSocket's TODOs

--- src/main/java/de/uniks/se1ss19teamb/rbsg/sockets/GameMessageHandler.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/sockets/GameMessageHandler.java
@@ -2,6 +2,6 @@ package de.uniks.se1ss19teamb.rbsg.sockets;
 
 import com.google.gson.JsonObject;
 
-interface GameMessageHandler {
+public interface GameMessageHandler {
     void handle(JsonObject response); //Dummy. Fill in Release 2
 }
