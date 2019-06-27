30dcf9e02a030e860ef6c547de83f9e2b498b21d
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-27 04:27:28 +0200

    Extend GameFieldController
    Add transition to InGame and make room to store the currently joined game.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameFieldController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameFieldController.java
@@ -3,15 +3,21 @@ package de.uniks.se1ss19teamb.rbsg.ui;
 import de.uniks.se1ss19teamb.rbsg.model.Game;
 import de.uniks.se1ss19teamb.rbsg.request.DeleteGameRequest;
 import de.uniks.se1ss19teamb.rbsg.request.JoinGameRequest;
+import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 
 public class GameFieldController {
+
+    @FXML
+    private HBox root;
 
     @FXML
     private Label gameNameLabel;
 
     private Game game;
+    static Game joinedGame;
 
 
     void setUpGameLabel(Game game) {
@@ -22,13 +28,15 @@ public class GameFieldController {
     public void joinGame() {
         JoinGameRequest joinGameRequest = new JoinGameRequest(game.getId(), LoginController.getUserKey());
         joinGameRequest.sendRequest();
-        MainController.joinedGame = game;
+        joinedGame = game;
+        UserInterfaceUtils.makeFadeOutTransition(
+            "/de/uniks/se1ss19teamb/rbsg/fxmls/inGame.fxml", root);
     }
 
     public void deleteGame() {
         DeleteGameRequest deleteGameRequest = new DeleteGameRequest(game.getId(), LoginController.getUserKey());
         deleteGameRequest.sendRequest();
-        MainController.updateGameView();
+        MainController.instance.updateGameView();
     }
 
 }
