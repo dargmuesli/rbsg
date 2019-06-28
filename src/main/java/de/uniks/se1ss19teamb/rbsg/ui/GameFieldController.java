bee661021f8ff756c2e4abce1e0cfb02474f4d49
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-06-28 18:49:28 +0200

    Correct Theming Of Subcontrollers

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameFieldController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameFieldController.java
@@ -3,10 +3,14 @@ package de.uniks.se1ss19teamb.rbsg.ui;
 import de.uniks.se1ss19teamb.rbsg.model.GameMeta;
 import de.uniks.se1ss19teamb.rbsg.request.DeleteGameRequest;
 import de.uniks.se1ss19teamb.rbsg.request.JoinGameRequest;
+import de.uniks.se1ss19teamb.rbsg.util.Theming;
 import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
+import java.util.Arrays;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
 
 public class GameFieldController {
 
@@ -19,6 +23,9 @@ public class GameFieldController {
     private GameMeta gameMeta;
     static GameMeta joinedGame;
 
+    public void initialize() {
+        Theming.setTheme(Arrays.asList(new Pane[]{root}));
+    }
 
     void setUpGameLabel(GameMeta gameMeta) {
         this.gameMeta = gameMeta;
