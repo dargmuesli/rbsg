1354b3329086ae5c4c935f833d9ce62a8c241563
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-07-12 03:11:22 +0200

    Army Model: Add Test

---
+++ src/test/java/de/uniks/se1ss19teamb/rbsg/model/ArmyTest.java
@@ -0,0 +1,56 @@
+package de.uniks.se1ss19teamb.rbsg.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArmyTest {
+    private String id = "123";
+    private String name = "myName";
+    private List<String> units = Arrays.asList("123", "456");
+    private String newId = "987";
+    private String newName = "myNewName";
+    private List<String> newUnits = Arrays.asList("456", "789");
+    private Army army;
+
+    @Before
+    public void prepareTests() {
+        army = new Army(id, name, units);
+    }
+
+    @Test
+    public void getIdTest() {
+        Assert.assertEquals(id, army.getId());
+    }
+
+    @Test
+    public void setIdTest() {
+        army.setId(newId);
+        Assert.assertEquals(newId, army.getId());
+    }
+
+    @Test
+    public void getNameTest() {
+        Assert.assertEquals(name, army.getName());
+    }
+
+    @Test
+    public void setNameTest() {
+        army.setName(newName);
+        Assert.assertEquals(newName, army.getName());
+    }
+
+    @Test
+    public void getUnitsTest() {
+        Assert.assertEquals(units, army.getUnits());
+    }
+
+    @Test
+    public void setUnitsTest() {
+        army.setUnits(newUnits);
+        Assert.assertEquals(newUnits, army.getUnits());
+    }
+}
