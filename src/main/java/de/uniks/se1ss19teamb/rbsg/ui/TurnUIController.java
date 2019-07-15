5809cd2cd0256fe10158a2dcd6861582f9ead30a
Christopher Bartalos <chrisibartalos96@web.de>
2019-07-15 10:22:43 +0200

    turn_ui
    added lines below names

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/TurnUIController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/TurnUIController.java
@@ -1,36 +1,29 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
-import animatefx.animation.AnimationFX;
-import animatefx.animation.Bounce;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.shape.Line;
 
-import java.util.ArrayList;
-
 public class TurnUIController {
 
     @FXML
-    Line linePlayerOne;
+    private Line linePlayerOne;
     @FXML
-    Line linePlayerTwo;
+    private Line linePlayerTwo;
     @FXML
-    Line linePlayerThree;
+    private Line linePlayerThree;
     @FXML
-    Line linePlayerFour;
+    private Line linePlayerFour;
     @FXML
-    Label labelOne;
+    private Label labelOne;
     @FXML
-    Label labelTwo;
+    private Label labelTwo;
     @FXML
-    Label labelThree;
+    private Label labelThree;
     @FXML
-    Label labelFour;
-
-    private ArrayList<Label> labels = new ArrayList<>();
+    private Label labelFour;
 
     public void initialize() {
-        labelOne.setText("jdbnbnfvodjfn");
         underLining(labelOne, linePlayerOne);
         underLining(labelTwo, linePlayerTwo);
         underLining(labelThree, linePlayerThree);
