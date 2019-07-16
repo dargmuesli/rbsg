c17d34671d86ba8bfd439acc2503b7170adc96f3
Rohhullah <rohhullah98@gmail.com>
2019-07-16 23:02:30 +0200

    other Version: merged new master

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameLobbyController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameLobbyController.java
@@ -4,8 +4,8 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXHamburger;
 import de.uniks.se1ss19teamb.rbsg.model.Army;
 import de.uniks.se1ss19teamb.rbsg.model.Unit;
-import de.uniks.se1ss19teamb.rbsg.model.units.*;
 import de.uniks.se1ss19teamb.rbsg.request.LogoutUserRequest;
+import de.uniks.se1ss19teamb.rbsg.request.QueryArmiesRequest;
 import de.uniks.se1ss19teamb.rbsg.sockets.GameSocket;
 import de.uniks.se1ss19teamb.rbsg.util.SerializeUtils;
 import de.uniks.se1ss19teamb.rbsg.util.Theming;
@@ -19,11 +19,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -73,143 +76,79 @@ public class GameLobbyController {
     @FXML
     private JFXButton btnMyReady;
 
-    private BazookaTrooper bazookaTrooper = new BazookaTrooper();
-    private Chopper chopper = new Chopper();
-    private HeavyTank heavyTank = new HeavyTank();
-    private Infantry infantry = new Infantry();
-    private Jeep jeep = new Jeep();
-    private LightTank lightTank = new LightTank();
-    private ArrayList<Unit> units = new ArrayList<>(Arrays.asList(bazookaTrooper, chopper,
-        heavyTank, infantry, jeep, lightTank));
+    private static final Path ARMY_SAVE_PATH =
+        Paths.get(System.getProperty("java.io.tmpdir") + File.separator + "rbsg_army-save-%d.json");
     private ArrayList<UnitConfigController> configControllers = new ArrayList<>();
-    private String armysavePath = "./src/main/resources/de/uniks/se1ss19teamb/rbsg/armySaves/armySave%d.json";
-    Army army = loadArmyConfig();
-    private ArrayList<Army> armies = new ArrayList<>();
-    private ArmyManagerController armyManagerController = new ArmyManagerController();
-    ArrayList<Integer> counts = new ArrayList<>();
 
 
     public void initialize() {
         UserInterfaceUtils.makeFadeInTransition(gameLobby);
         UserInterfaceUtils.updateBtnFullscreen(btnFullscreen);
 
-
         Theming.setTheme(Arrays.asList(new Pane[]{gameLobby, gameLobby1}));
         Theming.hamburgerMenuTransition(hamburgerMenu, btnBack);
         Theming.hamburgerMenuTransition(hamburgerMenu, btnLogout);
         Theming.hamburgerMenuTransition(hamburgerMenu, btnFullscreen);
-
-        showArmyConfig();
-        for (Unit unit : units) {
-            configControllers.get(0).showNumber(unit);
-            configControllers.get(1).showNumber(unit);
-        }
 
 
     }
 
 
     private void showArmyConfig() {
-        for (Unit unit : units) {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass()
-                .getResource("/de/uniks/se1ss19teamb/rbsg/fxmls/unitConfig.fxml"));
-            try {
-                Parent parent = fxmlLoader.load();
-                UnitConfigController configController = fxmlLoader.getController();
-                configController.loadConfig(unit, this);
-                configControllers.add(configController);
-                armyList.getItems().add(parent);
-                armyList.setOrientation(Orientation.HORIZONTAL);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass()
+            .getResource("/de/uniks/se1ss19teamb/rbsg/fxmls/unitConfig.fxml"));
+        try {
+            Parent parent = fxmlLoader.load();
+            UnitConfigController configController = fxmlLoader.getController();
+            configControllers.add(configController);
+            armyList.getItems().add(parent);
+            armyList.setOrientation(Orientation.HORIZONTAL);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-/*
-    ArrayList<Integer> numberOfUnits(Army army) {
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        int count4 = 0;
-        int count5 = 0;
-        int count6 = 0;
-
-        for (int i = 0; i < army.getUnits().size(); i++) {
-
-            if (bazookaTrooper.getId().equals(army.getUnits().get(i))) {
-                count1++;
-            }
-
-
-            if (chopper.getId().equals(army.getUnits().get(i))) {
-                count2++;
-            }
-
-            if (heavyTank.getId().equals(army.getUnits().get(i))) {
-                count3++;
-
-            }
-
-            if (infantry.getId().equals(army.getUnits().get(i))) {
-                count4++;
-
-            }
-
-            if (jeep.getId().equals(army.getUnits().get(i))) {
-                count5++;
-
-            }
-
-            if (lightTank.getId().equals(army.getUnits().get(i))) {
-                count6++;
-
-            }
-
-
-        }
-
-        counts.add(count1);
-        counts.add(count2);
-        counts.add(count3);
-        counts.add(count4);
-        counts.add(count5);
-        counts.add(count6);
-
-        return counts;
-    }
-
- */
 
 
     private Army loadArmyConfig() {
-        return SerializeUtils.deserialize(new File(String.format(armysavePath, 1)), Army.class);
+        return SerializeUtils.deserialize(new File(String.format(ARMY_SAVE_PATH.toString())), Army.class);
     }
 
 
     @FXML
-    public void setOnAction(ActionEvent event) {
+    private void eventHandler(ActionEvent event) {
         if (event.getSource().equals(btnBack)) {
-            switch (((JFXButton) event.getSource()).getId()) {
-                case "btnBack":
-                    GameSocket.instance.disconnect();
-                    UserInterfaceUtils.makeFadeOutTransition(
-                        "/de/uniks/se1ss19teamb/rbsg/fxmls/main.fxml", gameLobby);
-                    break;
-                default:
-            }
-        } else if (event.getSource().equals(btnFullscreen)) {
-            UserInterfaceUtils.toggleFullscreen(btnFullscreen);
-        } else if (event.getSource().equals(btnLogout)) {
-            LogoutUserRequest logout = new LogoutUserRequest(LoginController.getUser());
+            UserInterfaceUtils.makeFadeOutTransition(
+                "/de/uniks/se1ss19teamb/rbsg/fxmls/main.fxml", gameLobby);
+        }
+    }
+
+    @FXML
+    public void setOnAction(ActionEvent event) {
+        if (event.getSource().equals(btnLogout)) {
+            LogoutUserRequest logout = new LogoutUserRequest(LoginController.getUserKey());
             logout.sendRequest();
+
             if (logout.getSuccessful()) {
                 LoginController.setUserKey(null);
                 UserInterfaceUtils.makeFadeOutTransition(
                     "/de/uniks/se1ss19teamb/rbsg/fxmls/login.fxml", gameLobby);
             }
-        }
+        }else if (event.getSource().equals(btnFullscreen)) {
+            UserInterfaceUtils.toggleFullscreen(btnFullscreen);
+        }else if (event.getSource().equals(btnMyReady)) {
+            QueryArmiesRequest req = new QueryArmiesRequest(LoginController.getUserKey());
+            req.sendRequest();
+            VBox chatWindow = (VBox) gameLobby.getScene().lookup("#chatWindow");
+            JFXButton btnMinimize = (JFXButton) chatWindow.lookup("#btnMinimize");
+            btnMinimize.setDisable(false);
+
+            UserInterfaceUtils.makeFadeOutTransition("/de/uniks/se1ss19teamb/rbsg/fxmls/inGame.fxml",gameLobby);
+            gameLobby.getScene().lookup("#chatWindow");
     }
+}
+
 
 }
 
--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/UnitConfigController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/UnitConfigController.java
@@ -1,6 +1,6 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
-import de.uniks.se1ss19teamb.rbsg.model.Army;
+
 import de.uniks.se1ss19teamb.rbsg.model.Unit;
 import de.uniks.se1ss19teamb.rbsg.textures.TextureManager;
 import de.uniks.se1ss19teamb.rbsg.util.Theming;
@@ -8,8 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class UnitConfigController {
@@ -29,7 +27,7 @@ public class UnitConfigController {
     private Label labelUnitSave3;
 
     private GameLobbyController gameLobbyController;
-    private int count = 0;
+
 
     public void initialize() {
         Theming.setTheme(Arrays.asList(new Pane[]{unitConfig}));
@@ -41,31 +39,6 @@ public class UnitConfigController {
         imageUnitType.getChildren().add(TextureManager.getTextureInstance(unit.getType()));
     }
 
-
-    void showNumber(Unit unit) {
-        int counter = 0;
-        for (int i = 0; i < gameLobbyController.army.getUnits().size(); i++) {
-            if (unit.getId().equals(gameLobbyController.army.getUnits().get(0))) {
-                counter++;
-                labelUnitSave1.setText(Integer.toString(counter));
-            }
-        }
-    }
-
-
-
-    void numberUnits(Army army, ArrayList<Unit> units) {
-            for (int i = 0; i < army.getUnits().size(); i++) {
-                if (units.get(0).getId().equals(army.getUnits().get(i))) {
-                    count++;
-                    labelUnitSave1.setText(Integer.toString(count));
-                }
-                if (units.get(1).getId().equals(army.getUnits().get(i))) {
-
-                    labelUnitSave1.setText(Integer.toString(count));
-                }
-        }
-    }
 
 
 }
