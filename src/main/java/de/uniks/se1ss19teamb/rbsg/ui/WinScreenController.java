ce3b37a488f67bc9741d832f3da8a4a85820b6ea
Christopher Bartalos <chrisibartalos96@web.de>
2019-07-22 00:33:24 +0200

    winScreen
    added essential functions but didnt implement code since the gamelobby doesnt transfer the playernames to the inGameScreen

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/WinScreenController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/WinScreenController.java
@@ -1,4 +1,36 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+
 public class WinScreenController {
+    @FXML
+    private ImageView trophyPic;
+    @FXML
+    private Label winnerLabel;
+
+    private Boolean winnerFound = false;
+    private String winner;
+
+    public void initialize () {
+
+    }
+
+    public void checkWinner () {
+        if (winnerFound) {
+            trophyPic.setVisible(true);
+            winnerLabel.setText(winner);
+            winnerLabel.setVisible(true);
+        }
+    }
+
+    private void setWinnerToLabel () {
+        // winner = nameofWinner;
+    }
+
+    private void calculateWinner () {
+        winnerFound = true;
+    }
 }
