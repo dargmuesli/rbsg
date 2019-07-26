8f731d927b1eef69761e79de28776991f4b1f958
Vadim S <vadjka.92@mail.ru>
2019-07-26 02:03:36 +0200

    Fixing drag with open minimap
    The size of canvas was too big, changing width and height to 20. Changing the size of the minimap should be done using the size attribute.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/InGameController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/InGameController.java
@@ -122,7 +122,9 @@ public class InGameController {
 
         fillGameGrid();
 
-        miniMap = TextureManager.computeMinimap(environmentTiles, 100, 100, 5);
+        // changing width and height to heigher values makes the canvas of the minimap too big
+        // if you want to change the size of minimap please use the size parameter (or rework calculation)
+        miniMap = TextureManager.computeMinimap(environmentTiles, 20, 20, 8);
         miniMap.setVisible(false);
         inGameScreen.getChildren().add(miniMap);
 
