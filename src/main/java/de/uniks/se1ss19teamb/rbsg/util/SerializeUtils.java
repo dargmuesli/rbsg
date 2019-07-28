5de1f17b2b57b7d5789a91f0119546c0bbe58138
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-07-28 06:25:57 +0200

    Add File Chooser Utility Function
    Opens a file selection dialog and returns an optional file object.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/util/SerializeUtils.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/util/SerializeUtils.java
@@ -2,11 +2,29 @@ package de.uniks.se1ss19teamb.rbsg.util;
 
 import com.google.gson.Gson;
 import java.io.*;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.swing.*;
+
 public class SerializeUtils {
     private static final Logger logger = LogManager.getLogger();
+
+    public static Optional<File> chooseFile() {
+        JFrame parentFrame = new JFrame();
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select a file");
+
+        int userSelection = fileChooser.showSaveDialog(parentFrame);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            return Optional.of(fileChooser.getSelectedFile());
+        } else {
+            return Optional.empty();
+        }
+    }
 
     public static <T> T deserialize(File file, Class<T> myClass) {
         try (Reader reader = new FileReader(file)) {
