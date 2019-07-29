105d75b17988cc358720dd27e711cfb3e342c915
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-07-29 02:52:27 +0200

    Chuck Norris: Improve Linebreaks
    Less whitespace issues, better overview.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/features/ChuckNorrisJokeTicker.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/features/ChuckNorrisJokeTicker.java
@@ -6,18 +6,23 @@ import javafx.scene.control.Label;
 public class ChuckNorrisJokeTicker {
 
     private static String[] jokes = {
-        "Chuck Norris hat bis zur Unendlichkeit gezählt ... 2-mal.", "Chuck Norris kann schwarze "
-          + "Filzstifte nach Farbe sortieren.", "Chuck Norris hat alle Farben erfunden. Außer Rosa! Tom Cruise hat Rosa"
-          + " erfunden.","Einige Leute tragen Superman Schlafanzüge. Superman trägt Chuck Norris Schlafanzüge.", "Chuck"
-          + " Norris kann ein Feuer entfachen, indem er zwei Eiswürfel aneinander reibt.","Chuck Norris kann Drehtüren "
-          + "zuschlagen!", "Manche Menschen können viele Liegestützen — Chuck Norris kann alle.", "Chuck Norris wurde "
-          + "gestern geblitzt — beim Einparken", "Chuck Norris verzichtet auf seine Rechte — seine Linke ist sowieso "
-          + "schneller ...", "Chuck Norris kennt die letzte Ziffer von Pi.", "Chuck Norris trinkt seinen Kaffee am "
-          + "liebsten schwarz. Ohne Wasser.", "Chuck Norris wurde letztens von der Polizei angehalten ... — Die "
-          + "Polizisten sind mit einer Verwarnung davon gekommen.", "Chuck Norris ist Fallschirmspringen gegangen. Sein"
-          + " Fallschirm hat sich nicht geöffnet. Er ist den Fallschirm danach umtauschen gegangen.", "Arnold "
-          + "Schwarzenegger musste wegen schweren Verletzungen ins Krankenhaus eingeliefert werden. Chuck Norris hatte "
-          + "ihn auf Facebook angestupst."};
+        "Chuck Norris hat bis zur Unendlichkeit gezählt ... 2-mal.",
+        "Chuck Norris kann schwarze Filzstifte nach Farbe sortieren.",
+        "Chuck Norris hat alle Farben erfunden. Außer Rosa! Tom Cruise hat Rosa erfunden.",
+        "Einige Leute tragen Superman Schlafanzüge. Superman trägt Chuck Norris Schlafanzüge.",
+        "Chuck Norris kann ein Feuer entfachen, indem er zwei Eiswürfel aneinander reibt.",
+        "Chuck Norris kann Drehtüren zuschlagen!",
+        "Manche Menschen können viele Liegestützen — Chuck Norris kann alle.",
+        "Chuck Norris wurde gestern geblitzt — beim Einparken",
+        "Chuck Norris verzichtet auf seine Rechte — seine Linke ist sowieso schneller ...",
+        "Chuck Norris kennt die letzte Ziffer von Pi.",
+        "Chuck Norris trinkt seinen Kaffee am liebsten schwarz. Ohne Wasser.",
+        "Chuck Norris wurde letztens von der Polizei angehalten ... — Die Polizisten sind mit einer Verwarnung davon" +
+            " gekommen.",
+        "Chuck Norris ist Fallschirmspringen gegangen. Sein Fallschirm hat sich nicht geöffnet. Er ist den Fallschirm" +
+            " danach umtauschen gegangen.",
+        "Arnold Schwarzenegger musste wegen schweren Verletzungen ins Krankenhaus eingeliefert werden. Chuck Norris" +
+            " hatte ihn auf Facebook angestupst."};
     private static int random;
     private static AnimationTimer animationTimer;
 
