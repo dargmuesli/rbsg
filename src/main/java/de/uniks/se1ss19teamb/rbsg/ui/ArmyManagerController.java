a5c55f1b004c99743f7ed3cad2f524fb418de5eb
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-07-29 17:15:49 +0200

    ArmyManager: Lobby Selection
    Sets the army as selected in the lobby if lobby exists.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/ArmyManagerController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/ArmyManagerController.java
@@ -7,6 +7,7 @@ import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import de.uniks.se1ss19teamb.rbsg.model.Army;
 import de.uniks.se1ss19teamb.rbsg.model.Unit;
 import de.uniks.se1ss19teamb.rbsg.request.*;
+import de.uniks.se1ss19teamb.rbsg.sockets.GameSocket;
 import de.uniks.se1ss19teamb.rbsg.util.*;
 
 import java.io.IOException;
@@ -101,6 +102,10 @@ public class ArmyManagerController {
 
             txtfldArmyName.setText(army.getName());
             updateUnits();
+
+            if (GameLobbyController.instance != null) {
+                GameSocket.instance.changeArmy(army.getId());
+            }
         });
 
         txtfldArmyName.setOnKeyReleased(event -> {
