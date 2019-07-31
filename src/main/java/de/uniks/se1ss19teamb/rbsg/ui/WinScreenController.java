a82047deb9f419dc4166b908294ecb2f3442606b
Christopher Bartalos <chrisibartalos96@web.de>
2019-08-01 01:27:57 +0200

    winScreen
    travis fix
    also deleted else case

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/WinScreenController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/WinScreenController.java
@@ -4,13 +4,13 @@ import com.jfoenix.controls.JFXButton;
 import de.uniks.se1ss19teamb.rbsg.model.ingame.InGamePlayer;
 import de.uniks.se1ss19teamb.rbsg.sockets.GameSocket;
 import de.uniks.se1ss19teamb.rbsg.util.UserInterfaceUtils;
+import java.util.ArrayList;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
-import java.util.ArrayList;
 
 
 public class WinScreenController {
@@ -35,6 +35,7 @@ public class WinScreenController {
     }
 
     private void updatePlayer() {
+        // fills inGamePlayerList with InGamePlayers
         InGameController.inGameObjects.entrySet().stream()
             .filter(stringInGameObjectEntry -> stringInGameObjectEntry.getValue() instanceof InGamePlayer)
             .forEachOrdered(inGameObjectEntry -> {
@@ -46,8 +47,6 @@ public class WinScreenController {
         for (InGamePlayer player: inGamePlayerList) {
             if (player.getId().equals(winner)) {
                 winnerLabel.setText(player.getName());
-            } else {
-                return;
             }
         }
     }
