56af6e8a2557acf714a9e2ee368d957c6f9afd43
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-08-01 01:33:16 +0200

    Lobby: Player Readiness Color
    Now switches to green for players that are ready.

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/LobbyPlayerController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/LobbyPlayerController.java
@@ -3,11 +3,15 @@ package de.uniks.se1ss19teamb.rbsg.ui;
 import de.uniks.se1ss19teamb.rbsg.model.ingame.InGamePlayer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class LobbyPlayerController {
 
     @FXML
     private Label lblPlayerName;
+    @FXML
+    private Circle crclReadiness;
 
     private InGamePlayer inGamePlayer;
 
@@ -15,5 +19,6 @@ public class LobbyPlayerController {
         this.inGamePlayer = inGamePlayer;
         this.lblPlayerName.setText(inGamePlayer.getName());
         this.lblPlayerName.setId("inGamePlayer" + this.inGamePlayer.getColor());
+        this.crclReadiness.setFill(inGamePlayer.isReady() ? Color.GREEN : Color.RED);
     }
 }
