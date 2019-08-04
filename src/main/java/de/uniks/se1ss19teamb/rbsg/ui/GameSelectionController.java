58ad4e77d89a939d4fc7df7b33a9fae4b181e542
Birk Magnussen <birk.magnussen@online.de>
2019-08-04 16:27:47 +0200

    Unneeded Import

--- src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameSelectionController.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/ui/GameSelectionController.java
@@ -1,7 +1,5 @@
 package de.uniks.se1ss19teamb.rbsg.ui;
 
-import com.jfoenix.controls.JFXButton;
-
 import de.uniks.se1ss19teamb.rbsg.model.GameMeta;
 import de.uniks.se1ss19teamb.rbsg.request.DeleteGameRequest;
 import de.uniks.se1ss19teamb.rbsg.request.JoinGameRequest;
