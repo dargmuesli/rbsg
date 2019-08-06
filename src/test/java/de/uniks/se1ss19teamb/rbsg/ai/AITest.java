09fa8a7557e1c90e91185949d46bfa9d1782280c
Birk Magnussen <birk.magnussen@online.de>
2019-08-06 23:30:47 +0200

    Added Tests

---
+++ src/test/java/de/uniks/se1ss19teamb/rbsg/ai/AITest.java
@@ -0,0 +1,13 @@
+package de.uniks.se1ss19teamb.rbsg.ai;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AITest {
+	
+	@Test
+	public void testInstantiation() {
+		AI testInstance = AI.instantiate("", -1);
+		Assert.assertNotNull(testInstance);
+	}
+}
