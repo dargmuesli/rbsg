c01a2a7cbb950d16ab1863b2b9ed2fbfc27a61a7
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-08-15 16:34:49 +0200

    SoundManager: Test Quietly

--- src/test/java/de/uniks/se1ss19teamb/rbsg/sound/SoundManagerTest.java
+++ src/test/java/de/uniks/se1ss19teamb/rbsg/sound/SoundManagerTest.java
@@ -4,7 +4,6 @@ import de.uniks.se1ss19teamb.rbsg.TestUtil;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class SoundManagerTest {
@@ -20,16 +19,15 @@ public class SoundManagerTest {
         Assert.assertFalse(SoundManager.getLoaded("bgm"));
     }
 
-    @Ignore
     @Test
     public void checkLoadedSounds() throws InterruptedException {
         //Actually plays the sound and hopes for no exception
-        SoundManager.playSound("panzer", 0);
+        SoundManager.playSound("panzer", 0, 0);
         Thread.sleep(1000);
-        SoundManager.playSound("panzer", -1);
+        SoundManager.playSound("panzer", 0, -1);
         Thread.sleep(1000);
 
-        SoundManager.playSound("bgm", 0);
+        SoundManager.playSound("bgm", 0, 0);
         Thread.sleep(1000);
     }
 }
