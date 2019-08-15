0e8773a0fd6408e9313c2c1272fc55bb29439cea
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-08-15 16:31:49 +0200

    SoundManager: Optional Instance

--- src/main/java/de/uniks/se1ss19teamb/rbsg/sound/SoundManager.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/sound/SoundManager.java
@@ -2,72 +2,67 @@ package de.uniks.se1ss19teamb.rbsg.sound;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class SoundManager {
 
-    private static SoundManager instance = null;
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private static Optional<SoundManager> instance;
     private Map<String, Sound> sounds = new HashMap<>();
 
     private SoundManager() {
     }
 
     public static void init() {
-        instance = new SoundManager();
+        instance = Optional.of(new SoundManager());
 
-        instance.sounds.put("meme_bad_bitch", new Sound("bad_bitch.wav", true));
-        instance.sounds.put("meme_beer", new Sound("beer.wav", true));
+        instance.ifPresent(soundManager -> {
+            soundManager.sounds.put("meme_bad_bitch", new Sound("bad_bitch.wav", true));
+            soundManager.sounds.put("meme_beer", new Sound("beer.wav", true));
 
-        instance.sounds.put("BazookaTrooper", new Sound("bazooka1.wav",true));
-        instance.sounds.put("BazookaTrooper2", new Sound("bazooka2.wav",true));
-        //instance.sounds.put("BazookaTrooper_Move", new Sound(".wav",true));
+            soundManager.sounds.put("BazookaTrooper", new Sound("bazooka1.wav",true));
+            soundManager.sounds.put("BazookaTrooper2", new Sound("bazooka2.wav",true));
+            //instance.sounds.put("BazookaTrooper_Move", new Sound(".wav",true));
 
-        instance.sounds.put("Chopper", new Sound("chooper.wav",true));
-        //instance.sounds.put("Chopper_Move", new Sound(.wav",true));
+            soundManager.sounds.put("Chopper", new Sound("chooper.wav",true));
+            //instance.sounds.put("Chopper_Move", new Sound(.wav",true));
 
 
-        instance.sounds.put("Jeep", new Sound("jeep.wav",true));
-        //instance.sounds.put("Jeep_Move", new Sound(".wav",true));
+            soundManager.sounds.put("Jeep", new Sound("jeep.wav",true));
+            //instance.sounds.put("Jeep_Move", new Sound(".wav",true));
 
-        instance.sounds.put("LightTank", new Sound("lightTank.wav",true));
-        //instance.sounds.put("LightTank_Move", new Sound(".wav",true));
+            soundManager.sounds.put("LightTank", new Sound("lightTank.wav",true));
+            //instance.sounds.put("LightTank_Move", new Sound(".wav",true));
 
-        instance.sounds.put("Infantry", new Sound("infantry_fire.wav",true));
-        instance.sounds.put("Infantry_Move", new Sound("infantry_move.wav",true));
+            soundManager.sounds.put("Infantry", new Sound("infantry_fire.wav",true));
+            soundManager.sounds.put("Infantry_Move", new Sound("infantry_move.wav",true));
 
-        instance.sounds.put("HeavyTank",new Sound("panzer.wav", true));
-        //instance.sounds.put("HeavyTank_Move",new Sound(".wav", true));
+            soundManager.sounds.put("HeavyTank",new Sound("panzer.wav", true));
+            //instance.sounds.put("HeavyTank_Move",new Sound(".wav", true));
 
-        instance.sounds.put("nani", new Sound("nani.wav", true));
-        instance.sounds.put("Omae", new Sound("Omae_wa_mou_shindeiru.wav", true));
-        instance.sounds.put("Omae_nani", new Sound("Omae_wa_mou_shindeiru_nani.wav", true));
+            soundManager.sounds.put("nani", new Sound("nani.wav", true));
+            soundManager.sounds.put("Omae", new Sound("Omae_wa_mou_shindeiru.wav", true));
+            soundManager.sounds.put("Omae_nani", new Sound("Omae_wa_mou_shindeiru_nani.wav", true));
 
-        //https://www.youtube.com/watch?v=VZec_PqXXzo
-        //If anyone feels offended: Fight me
-        Sound bgm = new Sound("katyusha.mp3", false);
-        instance.sounds.put("bgm", bgm);
+            //https://www.youtube.com/watch?v=VZec_PqXXzo
+            //If anyone feels offended: Fight me
+            Sound bgm = new Sound("katyusha.mp3", false);
+            soundManager.sounds.put("bgm", bgm);
 
-        instance.sounds.put("missing", new Sound("silence.wav", true));
-        instance.sounds.put("missing2", new Sound("fail-trombone-01.wav", true));
+            soundManager.sounds.put("missing", new Sound("silence.wav", true));
+            soundManager.sounds.put("missing2", new Sound("fail-trombone-01.wav", true));
+        });
     }
 
     public static void playSound(String toFetch, float balance) {
-        if (instance != null) {
-            instance.fetchSound(toFetch).play(balance);
-        }
+        instance.ifPresent(soundManager -> soundManager.fetchSound(toFetch).play(balance));
     }
 
     public static boolean getLoaded(String toFetch) {
-        Sound sound = instance.fetchSound(toFetch);
-        return sound.keepInRam;
+        return instance.map(soundManager -> soundManager.fetchSound(toFetch).keepInRam).orElse(false);
     }
 
     private Sound fetchSound(String toFetch) {
-        Sound sound = sounds.get(toFetch);
-
-        if (sound == null) {
-            sound = sounds.get("missing");
-        }
-
-        return sound;
+        return sounds.getOrDefault(toFetch, sounds.get("missing"));
     }
 }
