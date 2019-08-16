bcbfe2cd24dda95363f295ffc6364c3cb1b64bd0
Jonas Thelemann <git-stats-ignore@jonas-thelemann.de>
2019-08-17 01:58:40 +0200

    Notifications: Correct Tests
    Using JUnit5 and TestFX.

--- src/test/java/de/uniks/se1ss19teamb/rbsg/util/RequestUtilTest.java
+++ src/test/java/de/uniks/se1ss19teamb/rbsg/util/RequestUtilTest.java
@@ -4,24 +4,37 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import de.uniks.se1ss19teamb.rbsg.Main;
 import de.uniks.se1ss19teamb.rbsg.request.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
-public class RequestUtilTest {
+@ExtendWith(ApplicationExtension.class)
+class RequestUtilTest {
     private HttpRequestResponse errorResponse =
         new HttpRequestResponse("{\"status\": \"error\"}", 200, null);
     private HttpRequestResponse successResponse =
         new HttpRequestResponse("{\"status\": \"success\", \"data\": {\"userKey\": \"123456\"}}", 200, null);
 
-    @Before
-    public void before() {
+    @Start
+    public void start(Stage stage) {
+        AnchorPane apn = new AnchorPane();
+        apn.setId("apnFade");
+        stage.setScene(new Scene(apn));
+        Main.PRIMARY_STAGE = stage;
+
         AbstractRestRequest.httpManager = mock(HttpManager.class);
     }
 
     @Test
-    public void requestBooleanTest() {
+    void requestBooleanTest() {
         try {
             when(AbstractRestRequest.httpManager.get(any(), any())).thenReturn(successResponse);
         } catch (Exception e) {
@@ -40,7 +53,7 @@ public class RequestUtilTest {
     }
 
     @Test
-    public void requestOptionalTest() {
+    void requestOptionalTest() {
         try {
             when(AbstractRestRequest.httpManager.post(any(), any(), any())).thenReturn(successResponse);
         } catch (Exception e) {
