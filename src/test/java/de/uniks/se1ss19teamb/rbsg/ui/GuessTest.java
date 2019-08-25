d2316e598d1217975ba55f4220756a257ee48b73
Rohhullah <rohhullah98@gmail.com>
2019-08-25 12:46:16 +0200

    Test

---
+++ src/test/java/de/uniks/se1ss19teamb/rbsg/ui/GuessTest.java
@@ -0,0 +1,32 @@
+package de.uniks.se1ss19teamb.rbsg.ui;
+
+import de.uniks.se1ss19teamb.rbsg.features.Guess;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+@ExtendWith(ApplicationExtension.class)
+public class GuessTest {
+    @Start
+    public void start(Stage stage) throws Exception {
+        Guess guess = new Guess();
+        guess.start(stage);
+
+    }
+
+    @Test
+    void guessTest(FxRobot robot) {
+        robot.sleep(2000);
+        robot.clickOn("#btnNewGame");
+        robot.sleep(200);
+        robot.clickOn("#numberField");
+        robot.write("1");
+        robot.clickOn("#btnStart");
+        robot.sleep(200);
+        robot.write("2");
+        robot.sleep(200);
+    }
+}
