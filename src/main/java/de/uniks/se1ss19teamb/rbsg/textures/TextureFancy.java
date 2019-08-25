35f8a6dcf9d012d938a8b8cae1ff8c54d49ddf46
0gain <gianluca.voss@gmx.de>
2019-08-25 16:11:16 +0200

    JavaDoc for TextureFancy

--- src/main/java/de/uniks/se1ss19teamb/rbsg/textures/TextureFancy.java
+++ src/main/java/de/uniks/se1ss19teamb/rbsg/textures/TextureFancy.java
@@ -4,6 +4,10 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
+/**
+ * A texture class for textures with multiple layers.
+ * User here mainly for Terrain Textures.
+ */
 class TextureFancy {
 
     private Texture base;
@@ -11,17 +15,34 @@ class TextureFancy {
     private Texture overlay;
     
     private int depth;
-    
+
+    /**
+     * Constructor of TextureFancy.
+     *
+     * @param classPathBase String of the path of the base, the lowest layer for the texture
+     * @param classPathOverlay String of the path of the overlay.
+     * @param depth The amount of overlays.
+     */
     TextureFancy(String classPathBase, String classPathOverlay, int depth) {
         base = new Texture(classPathBase);
         overlay = new Texture(classPathOverlay);
         this.depth = depth;
     }
-    
+
+    /**
+     * Base Pane getter.
+     * @return Pane of the base.
+     */
     Pane instantiateBase() {
         return base.instantiate(null);
     }
-    
+
+    /**
+     * Overlay Pane getter.
+     * @param position The position of the overlay fitting for the position of base.
+     * @param type Type of the base.
+     * @return Pane of the overlay.
+     */
     Pane instantiateOverlay(TextureFancyOverlayPosition position, TextureFancyOverlayType type) {
         int x = position.px;
         int y = position.py + type.offset;
@@ -33,6 +54,11 @@ class TextureFancy {
         return new Pane(iv);
     }
 
+    /**
+     * Standart getter.
+     *
+     * @return The amount of overlays.
+     */
     int getDepth() {
         return depth;
     }
